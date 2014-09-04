e78dafeea858a17e47c3ee17ac273e01228dee0c
Charles Borges de Lima <cborges13@gmail.com>
2014-09-04 16:27:24 -0300

    ultimo de aula, agora é contigo..

--- ListaDeVetor/Lista.hpp
+++ ListaDeVetor/Lista.hpp
@@ -12,17 +12,17 @@
 using namespace std;
 
 #define MAXLISTA 100
-#define ERROLISTACHEIA -1;
-#define ERROLISTAVAZIA -2;
-#define ERROPOSIÇÃO -3;
+#define ERROLISTACHEIA -1
+#define ERROLISTAVAZIA -2
+#define ERROPOSICAO -3
 
 template<typename T>
 class Lista
 {
 	private:
-		int ultimo;		
+		int ultimo;
 		int tamLista;
-		T *lista;
+		T *dados;
 
 	public:
 		Lista();
@@ -30,63 +30,257 @@ class Lista
 		void destroiLista();
 		bool listaCheia();
 		bool listaVazia();
-		///**********************
+		int adiciona(T dado);
+		T retira();
+		int adicionaNoInicio(T dado);
+		T retiraDoInicio();
+		int adicionaNaPosicao(T dado, int posicao);
+		T retiraDaPosicao(int posicao);
+		bool maior(T dado1,T dado2);
+		int  adicionaEmOrdem(T dado);
+		int posicao(T dado);
+		T  retiraEspecifico(T dado);
+		bool contem(T dado);
+		bool igual(T dado1,T dado2);
+		bool menor(T dado1,T dado2);
 
-
-		void AdicionaNoInício(T dado);
-		void AdicionaNaPosição(T dado, int posicao); 
-		void  AdicionaEmOrdem(T dado);
-		void Retira();
-		void RetiraDoInício();
-		void RetiraDaPosição(int posicao);
-		void  RetiraEspecífico(T dado);
-		
-
-
-		int Posicao(T dado);
-		bool Contem(T dado);
-		bool Igual(T dado1,T dado2); 
-		bool Maior(T dado1,T dado2);
-		bool Menor(T dado1,T dado2);
-		
-	
 };
 
-template <typename T>
-Lista<T>::Lista(){
+template<typename T>
+Lista<T>::Lista()
+{
 	tamLista = MAXLISTA;
-	lista = new T[tamLista];
+	dados = new T[tamLista];
 	ultimo = -1;
 }
 
-template <typename T>
-Lista<T>::Lista(int t){
+template<typename T>
+Lista<T>::Lista(int t)
+{
 	tamLista = t;
-	lista = new T[tamLista];
+	dados = new T[tamLista];
 	ultimo = -1;
 }
 
-template <typename T>
+template<typename T>
 void Lista<T>::destroiLista()
 {
 	ultimo = -1;
 }
 
-template <typename T>
+template<typename T>
 bool Lista<T>::listaCheia()
 {
-	if(ultimo==MAXLISTA-1)
+	if (ultimo == MAXLISTA - 1)
 		return true;
 	return false;
 }
 
-template <typename T>
+template<typename T>
 bool Lista<T>::listaVazia()
 {
-	if(ultimo==-1)
+	if (ultimo == -1)
 		return true;
 	return false;
 }
 
+template<typename T>
+int Lista<T>::adiciona(T dado)
+{
+	if (this->listaCheia())
+	{
+		throw ERROLISTACHEIA;
+	}
+	else
+	{
+		ultimo++;
+		dados[ultimo] = dado;
+		return ultimo;
+	}
+
+}
+
+template<typename T>
+T Lista<T>::retira()
+{
+	if (this->listaVazia())
+		throw ERROLISTAVAZIA;
+	else
+	{
+		ultimo--;
+		return (dados(ultimo + 1));
+	}
+
+}
+
+template<typename T>
+int Lista<T>::adicionaNoInicio(T dado)
+{
+	int posicao;
+
+	if (this->listaCheia())
+		throw ERROLISTACHEIA;
+	else
+	{
+		ultimo++;
+		posicao = ultimo;
+		while (posicao > 0)
+		{
+			dados[posicao] = dados[posicao - 1];
+			posicao--;
+		}
+		dados[0] = dado;
+		return 0;
+	}
+}
+
+template<typename T>
+T Lista<T>::retiraDoInicio()
+{
+	int posicao, valor;
+
+	if(this->listaVazia())
+	{
+		throw ERROLISTAVAZIA;
+	}
+	else
+	{
+		ultimo--;
+		valor = dados[0];
+		posicao=0;
+		while(posicao<=ultimo)
+		{
+			dados[posicao] = dados[posicao + 1];
+			posicao++;
+		}
+		return valor;
+	}
+}
+
+template<typename T>
+int Lista<T>::adicionaNaPosicao(T dado, int destino)
+{
+	int posicao;
+
+	if(this->listaCheia())
+		throw ERROLISTACHEIA;
+	else
+	{
+		if((destino > (ultimo + 1)) || (destino < 0))
+			throw ERROPOSICAO;
+		ultimo++;
+		posicao=ultimo;
+		while(posicao>destino)
+		{
+			 dados[posicao] = dados[posicao - 1];
+			 posicao--;
+		}
+		 dados[destino] = dado;
+		 return destino;
+	}
+}
+
+template<typename T>
+T Lista<T>::retiraDaPosicao(int fonte)
+{
+	int posicao, valor;
+
+	if((fonte > ultimo) || (fonte<0))
+		throw ERROPOSICAO;
+	else
+	{
+		if(this->listaVazia())
+			throw ERROLISTAVAZIA;
+		else
+		{
+			ultimo--;
+			valor=dados[fonte];
+			posicao = fonte;
+			while(posicao<=ultimo)
+			{
+				dados[posicao]=dados[posicao+1];
+				posicao++;
+			}
+			return valor;
+		}
+	}
+}
+
+template<typename T>
+bool Lista<T>::maior(T dado1, T dado2)
+{
+	if(dado1 > dado2)
+		return true;
+	return false;
+
+}
+
+template<typename T>
+int Lista<T>::adicionaEmOrdem(T dado)
+{
+	int posicao;
+
+	if(this-> listaCheia())
+		throw ERROLISTACHEIA;
+	else
+	{
+		posicao = 0;
+		while((posicao<= ultimo)&&(dado>dados[posicao]))
+			posicao++;
+		return this->adicionaNaPosicao(dado,posicao);
+	}
+
+}
+
+template<typename T>
+int Lista<T>::posicao(T dado)
+{
+	int posicao=0;
+
+	while((posicao<=ultimo) && !(igual(dado, dados[posicao])))
+		posicao++;
+	if(posicao>ultimo)
+		throw ERROPOSICAO;
+	else
+		return posicao;
+}
+
+template<typename T>
+T Lista<T>::retiraEspecifico(T dado)
+{
+	int posicao;
+
+	if(this->listaVazia())
+		throw ERROLISTAVAZIA;
+	else
+	{
+		posicao = this->posicao(dado);
+		if(posicao<0)
+			throw ERROPOSICAO;
+		else
+			return this->retiraDaPosicao(posicao);
+	}
+
+}
+
+
+//metodos nossos
+template<typename T>
+bool Lista<T>::contem(T dado)
+{
+
+}
+
+template<typename T>
+bool Lista<T>::igual(T dado1,T dado2)
+{
+
+}
+
+template<typename T>
+bool Lista<T>::menor(T dado1,T dado2)
+{
+
+}
 
 #endif /* LISTA_HPP_ */
