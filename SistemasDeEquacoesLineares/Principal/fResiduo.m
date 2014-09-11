e6d8be5542d5d385ccb887f015a4e8dc7ab236ee
evandrocoan <evandrocoan@hotmail.com>
2014-09-11 19:56:36 -0300

    Finais...

---
+++ SistemasDeEquacoesLineares/Principal/fResiduo.m
@@ -0,0 +1,11 @@
+function res = fResiduo(n, Aaux,x)
+	% Cálculo do resíduo
+	for i = 1: n
+	  aux = 0;
+	  for j = 1: n
+	    aux = aux + Aaux(i,j) * x(j);
+	  end
+	  res(i) = abs(aux - Aaux(i, n + 1));
+	end
+	res;
+end
