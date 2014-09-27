f1113826cccc11090a3854673b4ba2d92b234c43
evandrocoan <evandrocoan@hotmail.com>
2014-09-27 00:44:15 -0300

    Trabalhado melhor o algoritmo.

--- Trabalho 2 CCO/localizaRaiz.m
+++ Trabalho 2 CCO/localizaRaiz.m
@@ -1,25 +1,48 @@
 function [ xInicio xFinal ] = localizaRaiz( A, B ) 
   h = .1;
-  ir = 0;
+  ir = 1;
   
   % variável para o cálculo do comportamento da 
   % descontinuidade assintótica: x != pi / 2 + pi k
-  k = ir;
+  k = 0;
   a = A;
   b = a + h;
+
+  passos = 0;
+
+  % indice do perigo utilizado
+  p = 1;
+
+  %incrementa o intervalo para cálculo do perigo
+  % o ponto de perigo tem que ter uma resposta antes do inicio da calculo para
+  % assim quando se chegar perto dele, poder pular.
+  for ind = 1 : 11
+    %calculo o perigo da zona de divergência
+    perigos(ind) = ( pi / 2 ) + pi * ind
+  end
+
   %enquano bzinho não atingir o o bzão
-  while( b < B - h )
+  while( b < B - h & ir < 10 & passos < 100 )
+    passos = passos + 1;
+      
+    %verifica se uma raíz foi encontrada
     produto = f(a) * f(b);
-    perigo = ( pi / 2 ) + pi * k;
-    % ( perigo - h < a < perigo + h ) isola da zona de divergência
-    % produto < 0 informa que há uma raíz no intervalo
-    printf("( perigo - h > a )") ( perigo - h > a )
-    printf("( a > perigo + h )") ( a > perigo + h )
-    if( produto < 0 & ( perigo - h > a ) & ( a > perigo + h ) )
-      ir = ir + 1;
-      xInicio(ir) = a;
-      xFinal(ir) = b;
+
+    if( produto < 0 & ( perigos(p) - h > a ) )
+      % incrementa a raíz 
+      %ir++;
+      xInicio(ir) = a
+      xFinal(ir) = b
+      ir = ir + 1
+
+      % ajusta o perigo para o 'a' correto
+      if( perigos(p) < a )
+        %coloca o próximo perigo para o cálculo
+        p = p + 1;
+      end
     end
+
+    %faz seguir para o teste do próximo intervalo
     a = b;
     b = a + h;
   end
