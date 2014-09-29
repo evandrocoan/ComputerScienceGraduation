e8fdfae0937e2b89a4645272383441126796fb08
evandrocoan <evandrocoan@hotmail.com>
2014-09-29 12:35:57 -0300

    Lista duplamente encadeada completa.

--- ListaDuplamenteEncadeada/ListaDuplaEnc.hpp
+++ ListaDuplamenteEncadeada/ListaDuplaEnc.hpp
@@ -30,205 +30,206 @@
 template< typename T >
 class ListaDupla
 {
-    private:
-        /**
-         * Ponteiro para o primeiro elemento da lista
-         */ 
-        ElementoDuplo< T >* head;
+    
+private:
+    /**
+     * Ponteiro para o primeiro elemento da lista
+     */
+    ElementoDuplo< T >* head;
 
-        /**
-         * Número de elementos da lista
-         */
-        int size;
+    /**
+     * Número de elementos da lista
+     */
+    int size;
 
-    public:
-        
-        /**
-         * Construtor padrão que cria uma lista duplamente encadeada vazia
-         */
-        ListaDupla();
+public:
+    
+    /**
+     * Construtor padrão que cria uma lista duplamente encadeada vazia
+     */
+    ListaDupla();
 
-        /**
-         * Destrutor padrão que destrói os ponteiros da lista e marca o 
-         * tamanho da lista para zero
-         */
-        ~ListaDupla();
+    /**
+     * Destrutor padrão que destrói os ponteiros da lista e marca o 
+     * tamanho da lista para zero
+     */
+    ~ListaDupla();
 
-        /**
-         * Informa o tamanho atual da lista duplamente encadeada. 
-         * 
-         * @return um inteiro informando o tamanho da lista encadeada
-         */
-        int tamanho();
+    /**
+     * Informa o tamanho atual da lista duplamente encadeada. 
+     * 
+     * @return um inteiro informando o tamanho da lista encadeada
+     */
+    int tamanho();
 
-/*
-        // ****************************
-        ListaEnc< T >* criaLista();
-        // ****************************
-*/
-        /**
-         * Adiciona um dado recebido como parâmetro no início da lista
-         * 
-         * @param dado
-         */
-        void adicionaNoInicioDuplo( const T& dado );
-        
-        /**
-         * Retira o elemento da cabeça da lista encadeada
-         * 
-         * @return o elemento na cabeça da lista encadeada
-         */
-        T retiraDoInicioDuplo();
-        
-        /**
-         * Deleta o elemento no ínicio na cabeça da lista encadeada
-         * 
-         */
-        void eliminaDoInicioDuplo();
-        
-        /**
-         * Adiciona um elemento em uma posição específica.
-         * 
-         * @param dado a ser adicionado
-         * @param pos a posição para adicionar o dado
-         * @throw erro de posição se a posição é inexistente
-         */
-        void adicionaNaPosicaoDuplo( const T& dado, int pos );
-        
-        /**
-         * Retorna a posição de um elemento fornecido como parâmetro. 
-         * A posição nesta lista encadeada começa em 0, para o primeiro 
-         * elemento.
-         * 
-         * @param dado a ter a posição encontrada
-         * @return a posição do dado encontrado com um inteiro
-         */
-        int posicaoDuplo( const T& dado ) const;
-        
-        /**
-         * Descobre qual o ponteiro para um determinado elemento na lista.
-         * 
-         * @param dado a ser procurado um ponteiro
-         * @return o ponteiro para o dado passado como parâmetro
-         */
-        T* posicaoMemDuplo( const T& dado ) const;
-        
-        /**
-         * Informa se a lista encadeada contém o dado passado como parâmetro.
-         * 
-         * @param dado a ser encontrado
-         * @return true se a lista encadeada contém o dada passado como 
-         * parâmetro, false caso contrário.
-         */
-        bool contemDuplo( const T& dado );
-        
-        /**
-         * Elimina um elemento de uma certa posição nesta lista encadeada.
-         * 
-         * @param a posição do elemento a ser retirado
-         * @return o elemento retirado da posição
-         */
-        T retiraDaPosicaoDuplo( int pos );
+    /*
+     // ****************************
+     ListaEnc< T >* criaLista();
+     // ****************************
+     */
+    /**
+     * Adiciona um dado recebido como parâmetro no início da lista
+     * 
+     * @param dado
+     */
+    void adicionaNoInicioDuplo( const T& dado );
 
-        /**
-         * Adiciona um elemento no final da lista encadeada
-         * 
-         * @param dado a ser adicionado
-         */
-        void adicionaDuplo( const T& dado );
-        
-        /**
-         * Retira o elemento pertencente ao final da lista encadeada
-         * 
-         * @return o dado retirado da lista encadeada
-         */
-        T retiraDuplo();
+    /**
+     * Retira o elemento da cabeça da lista encadeada
+     * 
+     * @return o elemento na cabeça da lista encadeada
+     */
+    T retiraDoInicioDuplo();
 
-        /**
-         * Retira a primeiro ocorrência do dado fornecidado como parâmetro
-         * 
-         * @param dado a ter a primeira ocorrência retirada
-         * @return a primeira ocorrência do dado retirado
-         */
-        T retiraEspecificoDuplo( const T& dado );
-        
-        /**
-         * Adiciona um dado a lista encadeada, na ordem definida pelo tipo
-         * de objeto que essa lista encadeada reprensenta
-         * 
-         * @param dado a ser adicionada em ordem
-         */
-        void adicionaEmOrdem( const T& data );
-        
-        /**
-         * Informa se a lista encadeada está vazia
-         * 
-         * @return true se a lista encadeada está vazia, false caso contrário
-         */
-        bool listaVazia() const;
-        
-        /**
-         * Informa se dois dados são iguais. Utiliza o operador de igualdade 
-         * do tipo do objeto que essa lista encadeada representa para 
-         * determinar se eles são iguais.
-         * 
-         * @param dado1 a ser comparado
-         * @param dado2 a ser comparado
-         * @return true se os parametrôs são iguais, false caso contrário
-         */
-        bool igual( T dado1, T dado2 );
-        
-        /**
-         * Informa se o primeiro parâmetro é maior que o segundo. Utiliza o 
-         * operador de igualdade do tipo do objeto que essa lista encadeada 
-         * representa para determinar se eles são iguais.
-         * 
-         * @param dado1 a ser comparado
-         * @param dado2 a ser comparado
-         * @return true se o primeiro parâmetro é maior que o segundo, 
-         * false caso contrário
-         */
-        bool maior( T dado1, T dado2 );
-        
-        /**
-         * Informa se o primeiro parâmetro é menor que o segundo. Utiliza o 
-         * operador de igualdade do tipo do objeto que essa lista encadeada 
-         * representa para determinar se eles são iguais.
-         * 
-         * @param dado1 a ser comparado
-         * @param dado2 a ser comparado
-         * @return true se o primeiro parâmetro é menor que o segundo, 
-         * false caso contrário
-         */
-        bool menor( T dado1, T dado2 );
-        
-        /**
-         * Destrói todos os ponteiros da lista e ajusta seu tamanho para 0
-         */
-        void destroiListaDuplo();
-        
-        /**
-         * Informa se uma determinada posição válida.
-         * 
-         * @param a posição a ser verificada
-         * @return true se a posição é válida, false caso contrário
-         */
-        bool posicaoInvalida( int p );
-        
-        /**
-         * Mostra qual elemento está em uma posição específica.
-         * 
-         * @param posição do elemento a ser visto
-         * @return o elemento encontrado na posição requerida
-         */
-        T mostra(int pos);
-        
-        /**
-         * Mostra qual o último elemento da lista duplamente encadeada.
-         * 
-         * @return o último elemento da lista encadeada
-         */
-        int verUltimo();
+    /**
+     * Deleta o elemento no ínicio na cabeça da lista encadeada
+     * 
+     */
+    void eliminaDoInicioDuplo();
+
+    /**
+     * Adiciona um elemento em uma posição específica.
+     * 
+     * @param dado a ser adicionado
+     * @param pos a posição para adicionar o dado
+     * @throw erro de posição se a posição é inexistente
+     */
+    void adicionaNaPosicaoDuplo( const T& dado, int pos );
+
+    /**
+     * Retorna a posição de um elemento fornecido como parâmetro. 
+     * A posição nesta lista encadeada começa em 0, para o primeiro 
+     * elemento.
+     * 
+     * @param dado a ter a posição encontrada
+     * @return a posição do dado encontrado com um inteiro
+     */
+    int posicaoDuplo( const T& dado ) const;
+
+    /**
+     * Descobre qual o ponteiro para um determinado elemento na lista.
+     * 
+     * @param dado a ser procurado um ponteiro
+     * @return o ponteiro para o dado passado como parâmetro
+     */
+    T* posicaoMemDuplo( const T& dado ) const;
+
+    /**
+     * Informa se a lista encadeada contém o dado passado como parâmetro.
+     * 
+     * @param dado a ser encontrado
+     * @return true se a lista encadeada contém o dada passado como 
+     * parâmetro, false caso contrário.
+     */
+    bool contemDuplo( const T& dado );
+
+    /**
+     * Elimina um elemento de uma certa posição nesta lista encadeada.
+     * 
+     * @param a posição do elemento a ser retirado
+     * @return o elemento retirado da posição
+     */
+    T retiraDaPosicaoDuplo( int pos );
+
+    /**
+     * Adiciona um elemento no final da lista encadeada
+     * 
+     * @param dado a ser adicionado
+     */
+    void adicionaDuplo( const T& dado );
+
+    /**
+     * Retira o elemento pertencente ao final da lista encadeada
+     * 
+     * @return o dado retirado da lista encadeada
+     */
+    T retiraDuplo();
+
+    /**
+     * Retira a primeiro ocorrência do dado fornecidado como parâmetro
+     * 
+     * @param dado a ter a primeira ocorrência retirada
+     * @return a primeira ocorrência do dado retirado
+     */
+    T retiraEspecificoDuplo( const T& dado );
+
+    /**
+     * Adiciona um dado a lista encadeada, na ordem definida pelo tipo
+     * de objeto que essa lista encadeada reprensenta
+     * 
+     * @param dado a ser adicionada em ordem
+     */
+    void adicionaEmOrdem( const T& data );
+
+    /**
+     * Informa se a lista encadeada está vazia
+     * 
+     * @return true se a lista encadeada está vazia, false caso contrário
+     */
+    bool listaVazia() const;
+
+    /**
+     * Informa se dois dados são iguais. Utiliza o operador de igualdade 
+     * do tipo do objeto que essa lista encadeada representa para 
+     * determinar se eles são iguais.
+     * 
+     * @param dado1 a ser comparado
+     * @param dado2 a ser comparado
+     * @return true se os parametrôs são iguais, false caso contrário
+     */
+    bool igual( T dado1, T dado2 );
+
+    /**
+     * Informa se o primeiro parâmetro é maior que o segundo. Utiliza o 
+     * operador de igualdade do tipo do objeto que essa lista encadeada 
+     * representa para determinar se eles são iguais.
+     * 
+     * @param dado1 a ser comparado
+     * @param dado2 a ser comparado
+     * @return true se o primeiro parâmetro é maior que o segundo, 
+     * false caso contrário
+     */
+    bool maior( T dado1, T dado2 );
+
+    /**
+     * Informa se o primeiro parâmetro é menor que o segundo. Utiliza o 
+     * operador de igualdade do tipo do objeto que essa lista encadeada 
+     * representa para determinar se eles são iguais.
+     * 
+     * @param dado1 a ser comparado
+     * @param dado2 a ser comparado
+     * @return true se o primeiro parâmetro é menor que o segundo, 
+     * false caso contrário
+     */
+    bool menor( T dado1, T dado2 );
+
+    /**
+     * Destrói todos os ponteiros da lista e ajusta seu tamanho para 0
+     */
+    void destroiListaDuplo();
+
+    /**
+     * Informa se uma determinada posição válida.
+     * 
+     * @param a posição a ser verificada
+     * @return true se a posição é válida, false caso contrário
+     */
+    bool posicaoInvalida( int p );
+
+    /**
+     * Mostra qual elemento está em uma posição específica.
+     * 
+     * @param posição do elemento a ser visto
+     * @return o elemento encontrado na posição requerida
+     */
+    T mostra( int pos );
+
+    /**
+     * Mostra qual o último elemento da lista duplamente encadeada.
+     * 
+     * @return o último elemento da lista encadeada
+     */
+    int verUltimo();
 };
 
 /**
@@ -247,7 +248,6 @@ ListaDupla< T >::ListaDupla()
 template< typename T >
 ListaDupla< T >::~ListaDupla()
 {
-
 }
 
 /**
@@ -288,8 +288,8 @@ T ListaDupla< T >::retiraDoInicioDuplo()
     ElementoDuplo< T >* saiu = head;
     T volta = saiu->getInfo();
     head = saiu->getProximo();
-
-    if( head->getProximo( ) != NULL )
+    
+    if( head->getProximo() != NULL )
     {
         head->setAnterior( NULL );
     }
@@ -343,12 +343,6 @@ void ListaDupla< T >::adicionaNaPosicaoDuplo( const T& dado, int pos )
         return adicionaNoInicioDuplo( dado );
     }
     
-    // caso seja a última posição, chama o método que manipula a calda
-//    if( pos == size )
-//    {
-//        return adiciona
-//    }
-    
     // aloca um elemento no inicio da lista
     ElementoDuplo< T >* novo = new ElementoDuplo< T >( dado, 0, 0 );
     
@@ -361,16 +355,26 @@ void ListaDupla< T >::adicionaNaPosicaoDuplo( const T& dado, int pos )
     // salva o ponterio da cabeça da lista
     ElementoDuplo< T >* anterior = head;
     
-    //
+    // faz a troca dos elementos
     for( int i = 0; i < pos - 1; i++ )
     {
         anterior = anterior->getProximo();
     }
     
-    // 
+    // emenda a cabeça na lista (anterior) no novo elemento
     novo->setProximo( anterior->getProximo() );
     
+    //Se o novo não é o último da lista…
+    if( novo->getProximo() != NULL )
+    {
+        //Faço o antecessor do sucessor do novo.
+        novo->getProximo()->setAnterior( novo );
+    }
+    
     anterior->setProximo( novo );
+    
+    novo->setAnterior( anterior );
+    
     size++;
 }
 
@@ -466,7 +470,14 @@ T ListaDupla< T >::retiraDaPosicaoDuplo( int posicao )
             
             //Variável auxiliar para o dado retornado
             T volta = eliminar->getInfo();
+            
             anterior->setProximo( eliminar->getProximo() );
+            
+            if( eliminar->getProximo() != NULL )
+            {
+                eliminar->getProximo()->setAnterior( anterior );
+            }
+            
             size--;
             delete eliminar;
             return volta;
@@ -515,17 +526,22 @@ void ListaDupla< T >::adicionaEmOrdem( const T& dado )
     {
         return adicionaNoInicioDuplo( dado );
     }
+    
     ElementoDuplo< T >* atual = head;
     int posicao = 1;
-    while( atual->getProximo() != 0 && maior( dado, atual->getInfo() ) )
+    
+    while( atual->getProximo() != NULL && maior( dado, atual->getInfo() ) )
     {
+        //Encontrar posição para inserir.
         atual = atual->getProximo();
         posicao++;
     }
+    
     if( maior( dado, atual->getInfo() ) )
     {
         return adicionaNaPosicaoDuplo( dado, posicao + 1 );
     }
+    
     adicionaNaPosicaoDuplo( dado, posicao );
 }
 
@@ -577,18 +593,18 @@ bool ListaDupla< T >::posicaoInvalida( int p )
 }
 
 /* // ******************************************************************
-template< typename T >
-ListaEnc< T >* ListaDupla< T >::criaLista()
-{
-    ListaEnc* aLista;
-    aLista = new ListaEnc();
-    if( aLista != NULL )
-    {
-        aLista->size = 0;
-        aLista->head = NULL;
-    }
-    return aLista;
-}*/
+ template< typename T >
+ ListaEnc< T >* ListaDupla< T >::criaLista()
+ {
+ ListaEnc* aLista;
+ aLista = new ListaEnc();
+ if( aLista != NULL )
+ {
+ aLista->size = 0;
+ aLista->head = NULL;
+ }
+ return aLista;
+ }*/
 
 /**
  * \copydoc ListaDupla< T >::destroiListaDuplo()
@@ -605,12 +621,6 @@ void ListaDupla< T >::destroiListaDuplo()
         delete atual;
         
         this->size--;
-        
-        // evitar acessar um null pointer
-        if(atual != 0)
-        {
-           atual = atual->getProximo();
-        }
     }
 }
 
@@ -618,7 +628,7 @@ void ListaDupla< T >::destroiListaDuplo()
  * \copydoc T ListaDupla< T >::mostra(int pos)
  */
 template< typename T >
-T ListaDupla< T >::mostra(int pos)
+T ListaDupla< T >::mostra( int pos )
 {
     T elemento;
     
