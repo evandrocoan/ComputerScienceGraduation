43d604fd2ce25821b5ae0272f0bc5daf168c4155
João Marcus Alves <joaomarcalves@gmail.com>
2014-11-10 13:08:51 -0200

    Termiano parte de Maclaurin

--- Trabalho3CCO/mainInterpolRaiz.m
+++ Trabalho3CCO/mainInterpolRaiz.m
@@ -1,8 +1,3 @@
-﻿% Determine fun?s aproximadoras de f(x)=sqrt(x), com x entre 1 e 2, atrav?de Interpolador Polinomial, de s?es de MacLaurin, Chebyschev e Pad?encontre a melhor combina? 'n' e 'm' de Pad? com erro m?mo EXATO da ordem de 1e-6.
-% Lembre-se de primeiramente de fazer mudan?de vari?is de x entre [1;2] para t entre[-1;+1], fazendo todos as aproxima?s em t, e somente no final, retorne as vari?is de t para x, apresentando a forma final das s?es em fun? de x.
-% Imprima o erro m?mo em cada aproxima?.
-% Plote gr?cos comparativos das fun?s aproximadoras e dos erros em rela? ?un? exata.
-
 clc
 clear
 format long
@@ -11,16 +6,19 @@ format long
 a = 1;
 b = 2;
 
-n = 3				%numero de subdivis??do intervalo [a,b] (grau do polinomio)
-h = ( b - a ) / n;	% espa?ento entre as subdivis??x = a : h : b;		%vetor x de pontos para plotar o gr?co
-y = sqrt(x);		%vetor y de pontos para plotar o gr?co
+n = 3				%numero de subdivisoes do intervalo [a,b] (grau do polinomio)
+h = ( b - a ) / n;	%espacaento entre as subdivisao x = a : h : b;
+x = a : h : b;		%vetor x de pontos para plotar o gráfico		
+y = sqrt(x);		%vetor y de pontos para plotar o grafico
 
-%matriz que resulta de uma fun? diferen? divididas
+
+% 1. Interpolacao por Gregory-Newton
+%matriz que resulta de uma funcao de diferencas divididas
 difDiv = fDifDiv( n, x, y );
 difDiv(n+1,n) = 0;
 [x' y' difDiv ];
 
-%plotagem dos pontos 
+%plotagem dos pontos
 nPlotagem = 50 * n; %n intervalos dividos em 50 vezes
 aPlot = a;
 bPlot = b;
@@ -29,26 +27,27 @@ xPlot = aPlot: hPlotagem : bPlot; % 31 pontos para n = 3
 yE = sqrt(xPlot);
 
 for i = 1 : nPlotagem + 1
-	yPlot(i) = fPnGregNew( n, x, y, difDiv, xPlot(i) );
+	yGregNew(i) = fPnGregNew( n, x, y, difDiv, xPlot(i) );
 end
-erroGregNew = abs( yE .- yPlot );
+erroGregNew = abs( yE .- yGregNew );
+erroMaxGregNew = max(erroGregNew)
 
-ErroMaxInter = max(erroGregNew)
+% 2. Interpolação por series de Maclaurin
+nMac = 5
+cMac = fCoefMaclaurin( nMac, a, b ) %coeficientes da serie de Maclaurin em t=0
 
-%Aproxima? em s?es de Maclaurin com n = 14
-nMaclaurin = 5
-cMaclaurin = fCoefMaclaurin( nMaclaurin, a, b ); %coeficientes da s?e de Maclaurin em t
-%fMac(t) = c(1) + c(2) * t^1 + c(3) * t^2 + ...
-
-%plotagem dos pontos da s?e de Maclaurin
+%plotagem dos pontos da serie de Maclaurin
 for i = 1 : nPlotagem + 1
 	tP(i) = ( 2 * xPlot(i) - ( b+a ) ) / (b - a);
-	yMac(i) = fPnBrio( nMaclaurin, cMaclaurin, tP(i) );
+	yMac(i) = fPnBrio( nMac, cMac, tP(i) );
 end
+tP;
+xPlot;
+yMac;
 erroMac = abs( yE .- yMac );
 erroMaximoMac =  max( erroMac )
 
-plot(x, y, "b;f(x) = sqrt(x);", xPlot, yPlot, "g;g(x) = Pn(x) de Gregory-Newton;", xPlot, yMac, "r;g(x) = Pn(x) de serie de Maclaurin;");
-%plot( x, y, "*;f(x) = ln;", xP, yE, "r;f(x)=ln(x);", xP, yP, "b;g(x) = Pn(x) de Lagrange;", xP, yMac, "k;S?e de Maclaurin;" );
+ plot(x, y, "b;f(x) = sqrt(x);", xPlot, yGregNew, "g;g(x) = Pn(x) de Gregory-Newton;", xPlot, yMac, "r;g(x) = Pn(x) de serie de Maclaurin;");
 
-%plot( xPlot, erro, "k;Erro(x) = |f(x) - Pn(x)|;" );
+% plot(xPlot, erroGregNew, "b;Erro Gregory Newton;")	% Erro Gregory-Newton
+%plot(xPlot, erroMac, "r;Erro MacLaurin;");		% Erro Maclaurin
