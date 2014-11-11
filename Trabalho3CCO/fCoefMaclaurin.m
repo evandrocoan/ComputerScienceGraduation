6a0424e91dd936011562e7bf41d34cb168a37705
joaomarcalves <joaomarcalves@gmail.com>
2014-11-11 20:37:25 -0200

    100% do Chebyshev pronto

--- Trabalho3CCO/fCoefMaclaurin.m
+++ Trabalho3CCO/fCoefMaclaurin.m
@@ -7,7 +7,7 @@ function cMac = fCoefMaclaurin( nMaclaurin, a, b )
 	cMac(2) = (0.5) * ((0.5*(b+a))^(-(0.5))) * ((0.5*(b-a)));
 	
 	for i = 3 : nMaclaurin + 1
-		iO = (i-1)
+		iO = (i-1);
 		k = 2 * iO - 3;
 		
 		den = k * den;
@@ -18,6 +18,6 @@ function cMac = fCoefMaclaurin( nMaclaurin, a, b )
 		fi = ((-1)^i) * (mult) * ((0.5*(b+a))^(-(0.5*((2*iO)-1)))) * ((0.5*(b-a))^(iO));
 		fat = factorial(iO);
 		
-		cMac(i) = fi/fat
+		cMac(i) = fi/fat;
 	end
 end
