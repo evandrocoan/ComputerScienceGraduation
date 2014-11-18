76011367c3ca7db4298ca24331e08757ea6de098
evandrocoan <evandrocoan@hotmail.com>
2014-11-18 16:52:07 -0200

    Corrigido a formatação do código.

--- MutuamenteAmigos/amigos-paralelo.c
+++ MutuamenteAmigos/amigos-paralelo.c
@@ -2,52 +2,59 @@
 #include <stdlib.h>
 #include <math.h>       /* sqrt */
 
-int main(int argc, char **argv) {
-
-	int minimo = atoi(argv[1]);
-	int maximo = atoi(argv[2]);
-	int numThreads = atoi(argv[3]);
-	//printf("minimo: %d", minimo);
-	//printf("maximo: %d", maximo);
-	int range = maximo - minimo;
-	int i, j;
-	double amigos[range];
-	double fracaoA;
-
-	omp_set_num_threads(numThreads);
-		for (i = minimo; i <= maximo; i++) {
-			fracaoA = (double) somaDivisores(i) / i;
-			amigos[i - minimo] = fracaoA;
-		}
-
-		for(j = 0; j <= range; j++){
-
-		#pragma omp parallel for schedule(static)
-			for (i = j+1; i <= range; i++) {
-		    	if(amigos[j] == amigos[i]) {
-	        	   printf("Os numeros %d e %d são mutuamente amigos.\n",  (minimo +i), (minimo+j));
-		    	}
-			}
-
-	}
-	return 0;
+int main( int argc, char **argv )
+{
+    
+    int minimo = atoi( argv[ 1 ] );
+    int maximo = atoi( argv[ 2 ] );
+    int numThreads = atoi( argv[ 3 ] );
+    //printf("minimo: %d", minimo);
+    //printf("maximo: %d", maximo);
+    int range = maximo - minimo;
+    int i, j;
+    double amigos[ range ];
+    double fracaoA;
+    
+    omp_set_num_threads( numThreads );
+    for( i = minimo; i <= maximo; i++ )
+    {
+        fracaoA = (double) somaDivisores( i ) / i;
+        amigos[ i - minimo ] = fracaoA;
+    }
+    
+    for( j = 0; j <= range; j++ )
+    {
+        
+#pragma omp parallel for schedule(static)
+        for( i = j + 1; i <= range; i++ )
+        {
+            if( amigos[ j ] == amigos[ i ] )
+            {
+                printf( "Os numeros %d e %d são mutuamente amigos.\n",
+                        ( minimo + i ), ( minimo + j ) );
+            }
+        }
+        
+    }
+    return 0;
 }
 
-int somaDivisores(int valor) {
-	int extra = (int) sqrt(valor),j, divid, soma = 0;
-				for(j = 1; j <= extra; j++){
-					if(valor % j == 0) {
-						divid = valor/j;
-					//	printf("\nDivisores de %d: %d",valor, j);
-						//printf("\nDivisores de %d: %d",valor, valor/j);
-						if((valor/j) == j)
-							divid = 0;
-						soma += j + divid;
-
-					}
-				}
-				return soma;
-	}
-
-
+int somaDivisores( int valor )
+{
+    int extra = (int) sqrt( valor ), j, divid, soma = 0;
+    for( j = 1; j <= extra; j++ )
+    {
+        if( valor % j == 0 )
+        {
+            divid = valor / j;
+            //	printf("\nDivisores de %d: %d",valor, j);
+            //printf("\nDivisores de %d: %d",valor, valor/j);
+            if( ( valor / j ) == j )
+                divid = 0;
+            soma += j + divid;
+            
+        }
+    }
+    return soma;
+}
 
--- MutuamenteAmigos/amigos-sequencial-lento.c
+++ MutuamenteAmigos/amigos-sequencial-lento.c
@@ -2,39 +2,45 @@
 #include <stdlib.h>
 #include <math.h>       /* sqrt */
 
-int main(int argc, char **argv) {
-
-	int minimo = atoi(argv[1]);
-	int maximo = atoi(argv[2]);
+int main( int argc, char **argv )
+{
+    
+    int minimo = atoi( argv[ 1 ] );
+    int maximo = atoi( argv[ 2 ] );
 //	printf("minimo: %d", minimo);
-	//printf("maximo: %d", maximo);
-	int i, j;
-	double fracaoA, fracaoB;
-
-		for (i = minimo; i <= maximo; i++) {
-			fracaoA = (double) calculaSomaDosDivisores(i)/i;
-		for(j = i+1; j <= maximo; j++){
-			fracaoB = (double) calculaSomaDosDivisores(j)/j;
-		    	if(fracaoA == fracaoB) {
-	        	   printf("Os numeros %d e %d são mutuamente amigos.\n", (i), (j));
-			}
-		}
-	}
-
-	return -1;
+    //printf("maximo: %d", maximo);
+    int i, j;
+    double fracaoA, fracaoB;
+    
+    for( i = minimo; i <= maximo; i++ )
+    {
+        fracaoA = (double) calculaSomaDosDivisores( i ) / i;
+        for( j = i + 1; j <= maximo; j++ )
+        {
+            fracaoB = (double) calculaSomaDosDivisores( j ) / j;
+            if( fracaoA == fracaoB )
+            {
+                printf( "Os numeros %d e %d são mutuamente amigos.\n", ( i ),
+                        ( j ) );
+            }
+        }
+    }
+    
+    return -1;
 }
 
-int calculaSomaDosDivisores(int valor) {
-	int extra = (int) sqrt(valor),j, soma = 0;
-				for(j = 1; j <= extra; j++){
-					if(valor % j == 0) {
-					//	printf("\nDivisores de %d: %d",valor, j);
-						//printf("\nDivisores de %d: %d",valor, valor/j);
-						soma += j + (valor/j);
-					}
-				}
-				return soma;
-	}
-
-
+int calculaSomaDosDivisores( int valor )
+{
+    int extra = (int) sqrt( valor ), j, soma = 0;
+    for( j = 1; j <= extra; j++ )
+    {
+        if( valor % j == 0 )
+        {
+            //	printf("\nDivisores de %d: %d",valor, j);
+            //printf("\nDivisores de %d: %d",valor, valor/j);
+            soma += j + ( valor / j );
+        }
+    }
+    return soma;
+}
 
