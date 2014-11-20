c4eb33e73dc207438b385f14a6584e0d0adbd028
evandrocoan <evandrocoan@hotmail.com>
2014-11-20 02:05:46 -0200

    Questao1 completa.

--- P3/QUESTAO1.m
+++ P3/QUESTAO1.m
@@ -5,19 +5,20 @@ a = 1;
 b = 2;
 n = 0; %numero de subdivisões do intervalo [a,b] (grau do polinomio)
 passos = 0;
-ErroMax = 0;
+ErroMax = 1;
+bigOde10na4 = sqrt(10) * 1e-4
 
-while ( ErroMax < ( 3.16 * 1e-4 ) && ( passos < 1000 ) )
+while ( ( ErroMax > bigOde10na4 )  && ( passos < 1000 ) )
 	passos = passos + 1;
-	n = n + 1;
+	n = n + 1
 	h = ( b - a ) / n; % espaçamento entre as subdivisões
 	x = a : h : b; %vetor de pontos para plotar o gráfico
 	y = log(x);
 
 	%matriz que resulta de uma função diferenças divididas
-	difDiv = fDifDiv( n, x, y )
+	difDiv = fDifDiv( n, x, y );
 	difDiv(n+1,n) = 0;
-	[x' y' difDiv ]
+	[x' y' difDiv ];
 
 	%plotagem dos pontos 
 	nPlotagem = 50 * n; %3 intervalos dividos em 20 vezes
@@ -32,7 +33,7 @@ while ( ErroMax < ( 3.16 * 1e-4 ) && ( passos < 1000 ) )
 		%yP(i) = fPnLagrange( n, x, y, xP(i) );
 		yP(i) = fPnGregNew( n, x, y, difDiv, xP(i) );
 	end
-	erro = abs( yE .- yP )
+	erro = abs( yE .- yP );
 
 	ErroMax = max(erro)
 end
