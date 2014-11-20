790b7f3b6715abecdde5bb71e000277a49229d25
evandrocoan <evandrocoan@hotmail.com>
2014-11-20 02:01:16 -0200

    Questão um iniciada.

--- P3/QUESTAO1.m
+++ P3/QUESTAO1.m
@@ -3,10 +3,13 @@ clc
 clear
 a = 1;
 b = 2;
-n = 1; %numero de subdivisões do intervalo [a,b] (grau do polinomio)
+n = 0; %numero de subdivisões do intervalo [a,b] (grau do polinomio)
+passos = 0;
+ErroMax = 0;
 
-
-while
+while ( ErroMax < ( 3.16 * 1e-4 ) && ( passos < 1000 ) )
+	passos = passos + 1;
+	n = n + 1;
 	h = ( b - a ) / n; % espaçamento entre as subdivisões
 	x = a : h : b; %vetor de pontos para plotar o gráfico
 	y = log(x);
@@ -26,13 +29,13 @@ while
 	yE = log(xP);
 
 	for i = 1 : nPlotagem + 1
-	%yP(i) = fPnLagrange( n, x, y, xP(i) );
-	yP(i) = fPnGregNew( n, x, y, difDiv, xP(i) );
+		%yP(i) = fPnLagrange( n, x, y, xP(i) );
+		yP(i) = fPnGregNew( n, x, y, difDiv, xP(i) );
 	end
 	erro = abs( yE .- yP )
 
 	ErroMax = max(erro)
 end
 
-plot( x, y, "*;f(x) = ln;", xP, yE, "r;f(x)=ln(x);", xP, yP, "b;g(x) = Pn(x) de Lagrange;" );
+%plot( x, y, "*;f(x) = ln;", xP, yE, "r;f(x)=ln(x);", xP, yP, "b;g(x) = Pn(x) de Lagrange;" );
 %plot( xP, erro, "k;Erro(x) = |f(x) - Pn(x)|;" );
