749c9080db9c4d340caf1a766e12896ed2485450
evandrocoan <evandrocoan@hotmail.com>
2014-11-22 16:24:25 -0200

    Finalização do trabalho.

--- MutuamenteAmigos/concParal.c
+++ MutuamenteAmigos/concParal.c
@@ -38,14 +38,41 @@ for (i = 0; i < tam; i++)
  * @return soma dos divisores30
  */
 
-int calcula(int numero) {
-int acc = 1 + numero;
-int lim = numero/2;
-#pragma omp paralel for schedule(static) reduction(+: acc) shared(acc, lim)
-for (int i = 2; i <= lim; i++) {
-	if (numero % i == 0) {
-		acc += i;
+int calcula(int valor) {
+	// obtém o limite do for, que é a raíz quadrada do número
+	int extra = (int) sqrt( valor );
+	
+	// declara a variável para que servirá de indice para o for;
+	int j;
+	
+	// declara a variável que armazena o quociente, que, matematicamente, 
+	// também será um divisor do número 
+	int divid;
+	
+	// declara a variável que armazenará a soma dos dos divisores, e inicializa com 
+	// zero porque na primeira interação ela será preenchida carregado com os 
+	// divisores padrões de um número, 1 e ele próprio
+	int soma = 0;
+	
+	// define o inicio da execução paralela
+	//#pragma omp paralel for schedule(guided) reduction(+:soma) private(divid)
+	// interage até a raiz do número
+	for( j = 1; j <= extra; j++ )
+	{
+		//verifica-se se há um divisor do número
+		if( valor % j == 0 )
+		{
+			// armazena o quociente do divisor do número
+			divid = valor / j;
+			
+			// retira o quociente da soma quando se chega no último número da iteração
+			if( ( valor / j ) == j )
+			divid = 0;
+			
+			// utiliza a propriedade matemática que diz que os divisores e o quociente
+			// quando cálculados até a raíz de um número são representam seus dividores
+			soma += j + divid;
+		}
 	}
-}
- return acc;
+	return soma;
 }
