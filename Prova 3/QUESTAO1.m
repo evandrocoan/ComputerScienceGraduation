6fc3d4be7737a86562414d755dc41afb20815352
evandrocoan <evandrocoan@hotmail.com>
2014-11-27 16:31:11 -0200

    Concluido a primeira parte da questão 1. A atualizada a estrutura de arquivos.

--- Prova 3/QUESTAO1.m
+++ Prova 3/QUESTAO1.m
@@ -1,6 +1,7 @@
 %Interpolação polinomial por Gregory-Newton
 clc
 clear
+format long
 a = -1;
 b = 1;
 n = 0; %numero de subdivisões do intervalo [a,b] (grau do polinomio)
@@ -8,7 +9,7 @@ passos = 0;
 erroMax = 1;
 erroPermitido = sqrt(10) * 1e-4;
 
-while ((erroMax > erroPermitido))
+while ( (erroMax > erroPermitido) && ( passos < 100 ) ) 
 	passos = passos + 1;
 	n = n + 1;
 	
@@ -31,8 +32,10 @@ while ((erroMax > erroPermitido))
 	for i = 1 : nPlotagem + 1
 		yP(i) = fPnGregNew( n, x, y, difDiv, xP(i) );
 	end
-	erro = abs( yE .- yP );
-	erroMax = max(erro);
+	
+	%cálculo do erro por 
+	erro1 = abs( yE .- yP );
+	erroMax1 = max(erro);
 end
 
 erroMax
@@ -41,4 +44,48 @@ n
 %plot( xP, yE, "r;f(x) = cos(x);", xP, yP, "b;g(x) = Pn(x) de Gregory-Newton;" );
 plot( xP, erro, "k;Erro(x) = |f(x) - Pn(x)|;" );
 
-print Que1_G2.png
+
+
+%Aproximação por Séries Maclaurin
+clc
+clear
+format long
+a = -1;
+b = 1;
+n = 0; %numero de subdivisões do intervalo [a,b] (grau do polinomio)
+passos = 0;
+erroMax = 1;
+erroPermitido = sqrt(10) * 1e-6;
+
+while ( ( erroMax > erroPermitido )  && ( passos < 100 ) )
+	n += 1;
+	passos+=1;
+	
+	h = ( b - a ) / n; 
+	x = a : h : b;
+	y = cos(x);
+	
+	cMac = fCoefMaclaurin(n);
+	
+	
+	nPlot = 50 * n; 
+	aPlot = a;
+	bPlot = b;
+	hPlot = ( bPlot - aPlot ) / nPlot;
+	xP = aPlot: hPlot : bPlot; 
+	yE = f(xP);
+	
+	for i = 1 : nPlot + 1
+		yMac(i) = fPnBrio(n, cMac, xP(i));
+	end
+	
+	erroMac = abs( yE .- yMac );
+	erroMax =  max( erroMac );
+end
+
+n
+erroMax
+
+
+% plot( xP, yE, "b;f(x) = cos(x);", xP, yMac, "k;Série de Maclaurin;" );
+plot( xP, erroMac, "k;Erro(x) = |f(x) - Pn(x)|;" );
