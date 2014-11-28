39e1709ccafb73ead2810ae9430cc8c29fdfbce0
evandrocoan <evandrocoan@hotmail.com>
2014-11-28 21:34:05 -0200

    Removido espaços dos nomes do arquivos.

---
+++ Prova3/Version2/fRetroSubsMatlab.m
@@ -0,0 +1,21 @@
+function x = fRetroSubs(n, A)
+	% Resolução das variáveis	
+	if( abs( A(n, n) ) > 1.e-14 )
+		x(n) = A(n,n + 1) / A(n,n);
+	else
+		if( abs( A(n, n + 1) ) < 1.e-14 )		
+			x(n) = 1; % Valor particular de uma das infinitas soluções para o sistema indefinido
+		else
+			printf( '\nSistema impossivel\n\n' )
+			exit( 0 );
+		end
+	end
+	% "-1" significa decremento	
+	for i = n - 1: -1: 1 
+	  aux = 0;
+	  for j = i + 1: n
+	    aux = aux + A(i,j) * x(j);
+	  end
+	  x(i) = ( A(i, n + 1) - aux ) / A(i,i);
+	end
+end
