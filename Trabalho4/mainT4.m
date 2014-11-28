ae41d4f1e3b0ba55b468d0f0626a2f9b4cdc070f
Evandro Coan <evandrocoan@hotmail.com>
2014-11-28 20:21:13 +0000

    Inicio do trabalho

---
+++ Trabalho4/mainT4.m
@@ -0,0 +1,14 @@
+clc
+clear
+format long
+
+%Matriz desenvolvida nas Definições
+A=  [
+    [C1          C2          C3          2];
+    [C1*x1       C2*x2       C3*x3       0];
+    [C1*x1^2     C2*x2^2     C3*x3^3     2/3];
+    [C1*x1^3     C2*x2^3     C3*x3^3     0];
+    [C1*x1^4     C2*x2^4     C3*x3^4     2/5]; 
+    [C1*x1^5     C2*x2^5     C3*x3^5     0];
+    ]
+    
