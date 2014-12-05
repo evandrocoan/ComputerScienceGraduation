90e2893f02e998d88b3025eb841a449964ff9417
evandrocoan <evandrocoan@hotmail.com>
2014-12-05 00:41:42 -0200

    Removidos arquivos desnecessários.

--- Prova 3/Version2/fPnBrio.m
+++
@@ -1,8 +0,0 @@
-% dado xP calcula yP = Pn(x = xP) por Briot-Ruffini
-function yP = fPnBrio( n, coef, xP ) 
-	b(n+1) = coef(n + 1);
-	for i = n : -1 : 1
-		b(i) = coef(i) + xP * b(i + 1);
-	end
-	yP = b(1);
-end
