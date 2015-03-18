6241b85321e2794b3eb5e45870ec5108e7b7021f
evandrocoan <evandrocoan@hotmail.com>
2015-03-18 19:46:28 -0300

    Created demonstration of inventary access at the driver class.

--- DriverClass.java
+++ DriverClass.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+
 /**
  * Esta classe serve como utiliário que realiza uma serie de test para com a
  * primeira interação do HomeBroker.
@@ -15,16 +16,50 @@ public class DriverClass
      */
     public static void main( String[] args )
     {
-        String nome = JOptionPane.showInputDialog("Qual é o seu nome?");
+        String command =
+                new String( "Bem-vindo ao sistema "
+                        + "tabajara de cadastro de ações\n "
+                        + "Digite 'sair' para fechar o programa.\n"
+                        + "Digite 'v' para para ver o inventario\n"
+                        + "Digite 'c' para para criar uma conta!\n" );
+        JOptionPane.showMessageDialog( null, command );
+        
+        // ######################## Cria um conta para apresentação do sistema
+        // ao usuario ########################################################
         Conta contaTeste =
                 new Conta( "Evandro", "123", 2000.5, true, new Inventario() );
         
         contaTeste.getInventario().adicionarAoInventario(
-                new Acao( 20.2, 1000, "Tabajara SA" ) );
-        String teste = contaTeste.getInventario().inventarioToString();
+                new Acao( 2.2, 10, "Tabajara SA" ) );
+        contaTeste.getInventario().adicionarAoInventario(
+                new Acao( 22.2, 100, "Tabajara SO" ) );
+        contaTeste.getInventario().adicionarAoInventario(
+                new Acao( 200.2, 1000, "Tabajara SP" ) );
+        contaTeste.getInventario().adicionarAoInventario(
+                new Acao( 2000.2, 10000, "Tabajara ST" ) );
+        contaTeste.getInventario().adicionarAoInventario(
+                new Acao( 200006.2, 10000, "Tabajara SS" ) );
         
-        System.out.println( teste + " / " + nome);
-        
-        
+        while( !command.equals( "sair" ) )
+        {
+            command = JOptionPane.showInputDialog( "Selecione o comando" );
+            
+            switch( command )
+            {
+            case "sair":
+                return;
+            case "v":
+                String teste = contaTeste.getInventario().inventarioToString();
+                JOptionPane.showMessageDialog( null, teste );
+                break;
+            case "c":
+                String nome = JOptionPane.showInputDialog( "Digite seu nome:" );
+                String senha =
+                        JOptionPane.showInputDialog( "Digite sua senha:" );
+                Conta c = new Conta( nome, senha, 0, false, new Inventario() );
+                // ( String nome, String senha, double saldo,boolean
+                // administrador, Inventario inventario )
+            }
+        }
     }
 }
