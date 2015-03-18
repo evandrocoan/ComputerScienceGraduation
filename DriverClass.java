89c0369598a05f30c0a876e48ac1fb0c6a6aa60d
evandrocoan <evandrocoan@hotmail.com>
2015-03-18 15:29:07 -0300

    Driver class created.

---
+++ DriverClass.java
@@ -0,0 +1,20 @@
+/**
+ * Esta classe serve como utiliário que realiza uma serie de test para com a
+ * primeira interação do HomeBroker.
+ * 
+ * @authors Evandro  Coan, Renan Pinho Assi
+ */
+public class DriverClass
+{
+    
+    /**
+     * Método principal que inicia a execução dos testes
+     * 
+     * @param args os argumentos passados por linha de comando
+     */
+    public static void main( String[] args )
+    {
+        
+    }
+    
+}
