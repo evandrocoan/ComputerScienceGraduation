fac34d957b78fb70005f5d16a301f379451ab31c
evandrocoan <evandrocoan@hotmail.com>
2015-03-27 21:06:11 -0300

    Criado um teste para analizar o comportamento das interfaces gráficas da API java.

---
+++ testes/DialogTest.java
@@ -0,0 +1,64 @@
+/**
+ * Pacote que contém a classe principal de testes.
+ */
+package testes;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * @author Professional
+ *
+ */
+public class DialogTest
+{
+    static final SimpleDateFormat SDF = new SimpleDateFormat(
+            "MM/dd/yyyy hh:mm:ss" );
+    
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        
+        final JFrame frame = new JFrame( "Dialog test" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.add( createPanelToPopDialog( frame ) );
+        frame.setSize( 500, 200 );
+        frame.setVisible( true );
+    }
+    
+    static JPanel createPanelToPopDialog( final JFrame parent )
+    {
+        JPanel panel = new JPanel();
+        panel.setLayout( new BorderLayout() );
+        JButton button = new JButton( "Pop a Dialog" );
+        button.addActionListener( new ActionListener()
+        {
+            @Override
+            public void actionPerformed(
+                    @SuppressWarnings( "unused" ) ActionEvent e )
+            {
+                JDialog dialog = new JDialog( parent, true );
+                dialog.add( createPanelToPopDialog( parent ) );
+                dialog.setSize( 500, 200 );
+                dialog.setVisible( true );
+            }
+        } );
+        panel.add( button, BorderLayout.SOUTH );
+        panel.add( new JLabel( "Created at " + SDF.format( new Date() ) ) );
+        
+        panel.setBorder( BorderFactory.createEmptyBorder( 10, 10, 10, 10 ) );
+        return panel;
+    }
+}
