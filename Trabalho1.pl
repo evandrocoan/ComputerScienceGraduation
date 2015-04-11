f2b23a98f948ea3100319e05f01b478bac556138
evandrocoan <evandrocoan@hotmail.com>
2015-04-11 02:04:44 -0300

    Introduzido o cabeçalho da questão 8.

--- Trabalho1.pl
+++ Trabalho1.pl
@@ -77,19 +77,19 @@ informacoesPessoais( ['Pessoa9 da Silva1', date( 1976, 11, 20 ),
 'Florianopolis', 4887347425 ] ).
 
 
-/* Questão 1
+/* Questão 1 ###############################################################
  * Qual o telefone de uma dada pessoa?
  * Primeiro testo se a pessoa é a cabeça da lista.
  * Segundo pego o telefone dela nesta lista na quarta posição.
  * */
 qualTelefoneDe(Nome,Telefone) :- informacoesPessoais([Nome, _, _, Telefone]).
 
-/* Questão 2
+/* Questão 2 ###############################################################
  * Quais as pessoas de uma dada cidade?
  * */
 quemMoraEm(Cidade, Nome) :- informacoesPessoais([Nome, _, Cidade, _]).
 
-/* Questão 3
+/* Questão 3 ###############################################################
  * Qual a idade de uma dada pessoa?
  * Primeiro, converto a data de aniversário para um TimeStamp. 
  * Segundo, consigo a data atual e armazeno em outro TimeStamp2.
@@ -114,7 +114,7 @@ qualIdadeDe(Nome, Idade) :-
 	privado_DadoNaPosicao(DataDeNascimento,[_|L],2),
 	privado_CalcularIdade(DataDeNascimento, Idade).
 	
-/* Questão 4
+/* Questão 4 ###############################################################
  * Quais as pessoas com mais de 30 anos?
  * Primeiro, carrego a Lista com os dados da pessoa.
  * Segundo, calculo a idade da pessoa.
@@ -128,7 +128,7 @@ quaisComMaisDe30Anos(Nome) :-
 	Idade > 30,
 	privado_DadoNaPosicao(Nome,[_|L],1).
 
-/* Questão 5
+/* Questão 5 ###############################################################
  * Quais os cursos de uma dada pessoa?
  * Primeiro encontro a pessoa na lista.
  * Segundo pego o curso dela e retorna na variável Curso.
@@ -138,7 +138,7 @@ qualCursoDe(Nome, Curso) :-
 	DadoDeRetorno = Nome,
 	privado_DadoNaPosicao(Curso,[_|L],2).
 
-/* Questão 6 
+/* Questão 6 ###############################################################
  * Quais os(as) orientadores(as) de uma dada pessoa?
  * Primeiro, pega a lista.
  * Segundo, pega o nome da pessoa e verifica se é a pessoa que queremos.
@@ -150,7 +150,7 @@ quaisOrientadoresDe(Nome, Orientadores) :-
 	NomeDaPessoa = Nome,
 	privado_DadoNaPosicao(Orientadores, [_|L], 4).
 
-/* Questão 7
+/* Questão 7 ###############################################################
  * Quais os colegas de curso ou de trabalho de uma dada pessoa?
  * Primeiro, tiro a parte inicial da lista.
  * Segundo, imprimo o restante da lista, isto é, o nome das referências.
@@ -176,6 +176,9 @@ quaisColegasDe(Nome, Colegas) :-
 	privado_is_head_member(Nome, Lista), 
 	privado_DividirLista(Lista, 6, _, Colegas).
 
+/* Questão 8 ###############################################################
+ * Quais as pessoas sem nenhum colega citado como referencia?
+ * */
 
 
 
