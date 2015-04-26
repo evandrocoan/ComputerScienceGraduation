a3a6e8e53742e6dc8ac8abf9cf0fd0a82b16c9dd
evandrocoan <evandrocoan@hotmail.com>
2015-04-25 21:12:26 -0300

    Impedido de criar instâncias da classe de testes.

--- homebroker/testes/DriverClass.java
+++ homebroker/testes/DriverClass.java
@@ -10,15 +10,22 @@ import homebroker.interface_gráfica.Homebroker;
  * 
  * @authors Evandro  Coan, Renan Pinho Assi
  */
-public class DriverClass
+public final class DriverClass
 {
     /**
      * Método principal que inicia a execução dos testes
      * 
      * @param args os argumentos passados por linha de comando
      */
-    public static void main( String... args )
+    public static void main( final String... args )
     {
         Homebroker.main( "teste" );
+    }
+    
+    /**
+     * Torna esse uma classe de utilizades, impedindo sua instânciação.
+     */
+    private DriverClass()
+    {
     }
 }
