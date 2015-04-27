c13dd3a65cb5c403365751ea0202c5e4c5a77b4e
evandrocoan <evandrocoan@hotmail.com>
2015-04-26 23:34:56 -0300

    Adicionado suporte a tentativa de utilizar uma conta não carregado ao criar uma ordem de venda.

--- homebroker/lógica_de_execução/MotorDoHomebroker.java
+++ homebroker/lógica_de_execução/MotorDoHomebroker.java
@@ -3,12 +3,8 @@
  */
 package homebroker.lógica_de_execução;
 
-import homebroker.lógica_de_dados.Ação;
 import homebroker.lógica_de_dados.Conta;
-import homebroker.lógica_de_dados.Inventario;
-import homebroker.util.Biblioteca;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -43,7 +39,7 @@ public final class MotorDoHomebroker
      * As contasTeste que serão utilizadas para simular a adição de contas no
      * sistema, isto é, as contas criadas somente existirão temporariamente.
      */
-    private transient List< Conta > contasTeste;
+    public transient List< Conta > contasTeste;
     
     /**
      * A conta para qual se estará operando o inventário e no merdado de ações.
@@ -74,66 +70,7 @@ public final class MotorDoHomebroker
         processoDoBook.start();
         
         // Cria contas fictícias
-        this.criarContasFicticia( 30, "123" );
-    }
-    
-    /**
-     * Transforma um ArrayList de contas e uma String
-     * 
-     * @param contas um ArrayList contendo as contas
-     * @return texto um texto contendo os nomes das contas de teste criadas
-     */
-    private String contasTesteToString()
-    {
-        final StringBuffer texto = new StringBuffer();
-        
-        int contador = 0;
-        for( final Conta conta: this.contasTeste )
-        {
-            texto.append( conta.getNome() );
-            texto.append( ", " );
-            if( Biblioteca.quebrarLinha( contador ) )
-            {
-                texto.append( "\n" );
-            }
-            contador = contador + 1;
-        }
-        return texto.toString();
-    }
-    
-    /**
-     * Cria contas teste para o sistema.
-     * 
-     * @param quantidade a quantidade de contas teste para se criar
-     * @param senha senha que as contas de teste terão
-     */
-    @SuppressWarnings( "all" )
-    private void criarContasFicticia( final int quantidade, final String senha )
-    {
-        final ArrayList< Conta > contasTeste = new ArrayList<>();
-        contasTeste.add( new Conta( "admin", "admin", 2000.5 * Biblioteca
-            .gerarNumeroAleatorio(), true, new Inventario() ) );
-        
-        MotorDoHomebroker.criarInventarioFicticio( contasTeste.get( 0 ),
-            quantidade );
-        
-        Conta contaTeste;
-        
-        for( int i = 0; i < quantidade; i++ )
-        {
-            contaTeste = new Conta( "User" + Biblioteca.gerarNumeroAleatorio(),
-                senha, 2000.5 * Biblioteca.gerarNumeroAleatorio(), false,
-                new Inventario() );
-            MotorDoHomebroker.criarInventarioFicticio( contaTeste, quantidade );
-            
-            contasTeste.add( contaTeste );
-        }
-        if( MotorDoHomebroker.LOG.isLoggable( Level.SEVERE ) )
-        {
-            MotorDoHomebroker.LOG.severe( "Estou em criarContasFictícias "
-                + contasTeste.get( 0 ).getNome() );
-        }
-        this.contasTeste = contasTeste;
+        this.contasTeste = UtiliárioDeContas.criarContasFicticia( 30, "123" );
     }
     
     /**
@@ -154,6 +91,12 @@ public final class MotorDoHomebroker
     
     private void efetuarVendaDeAção()
     {
+        if( this.contaAutenticada == null )
+        {
+            JOptionPane.showMessageDialog( null, "Não há "
+                + "nenhuma conta carregada no sistema!" );
+            return;
+        }
         boolean sucesso = false;
         
         while( !sucesso )
@@ -219,8 +162,8 @@ public final class MotorDoHomebroker
     {
         final String imput = JOptionPane.showInputDialog(
             "Insira o preço da ação:", Double
-            .toString( this.contaAutenticada
-                    .getAçãoPreço( açãoParaVender ) ) );
+                .toString( this.contaAutenticada
+                .getAçãoPreço( açãoParaVender ) ) );
         if( imput == null )
         {
             return 0;
@@ -257,31 +200,13 @@ public final class MotorDoHomebroker
     }
     
     /**
-     * Método de realiza o login no sistema.
+     * Verifica se as informações de login saõ válidas.
      * 
-     * @param darDica uma dica que será aprensetada no menu do login.
-     *            Inicialmente ela serve para exibir quais contas estão
-     *            disponiveis para login e sua senha
+     * @param usuário o nome de usuário.
+     * @param senha a senha ser verificada.
+     * @return true caso seja autenticado a conta
      */
-    
-    public void loginNoSistema( final boolean darDica )
-    {
-        if( darDica )
-        {
-            final StringBuilder dica = new StringBuilder();
-            dica.append( "\n" );
-            dica.append( this.contasTesteToString() );
-            this.loginNoSistemaInterno( dica.toString() );
-        } else
-        {
-            this.loginNoSistemaInterno( "" );
-        }
-    }
-    
-    /**
-     * 
-     */
-    private boolean loginNoSistemaChecagem( final String usuário,
+    public boolean loginNoSistemaChecagem( final String usuário,
         final String senha )
     {
         for( final Conta conta: this.contasTeste )
@@ -293,41 +218,6 @@ public final class MotorDoHomebroker
             }
         }
         return false;
-    }
-    
-    /**
-     * 
-     */
-    private void loginNoSistemaInterno( final String dica )
-    {
-        String usuário = "";
-        String senha = "";
-        boolean inputError = true;
-        do
-        {
-            usuário = JOptionPane.showInputDialog(
-                ( inputError? "" : "Usuário ou senha inválidos\n\n" )
-                + "Insira qual conta será feito login: " + dica );
-            
-            if( ( usuário == null ) )
-            {
-                break;
-            }
-            senha = JOptionPane.showInputDialog( "Insira qual senha para a "
-                + "conta: " + usuário );
-            
-            if( ( senha == null ) )
-            {
-                break;
-            }
-            inputError = this.loginNoSistemaChecagem( usuário, senha );
-            
-        } while( !inputError );
-        
-        if( ( usuário == null ) | ( senha == null ) )
-        {
-            System.exit( 0 );
-        }
     }
     
     /**
@@ -351,10 +241,10 @@ public final class MotorDoHomebroker
         case "v":
             this.mostrarInventário();
             break;
-        // case "c":
-        // TODO
-        // this.criarUsuario();
-        // break;
+            // case "c":
+            // TODO
+            // this.criarUsuario();
+            // break;
         case "ov":
             this.efetuarVendaDeAção();
             break;
@@ -380,52 +270,11 @@ public final class MotorDoHomebroker
     }
     
     /**
-     * Cria um inventário fictício de ações contendo 5 ações fictícias.
-     * 
-     * @param conta a conta que irá receber as ações fictícioas.
-     * @param quantidade a quantidade de ações fictícias para se criar.
-     */
-    @SuppressWarnings( { "all" } )
-    private static void criarInventarioFicticio( final Conta conta,
-        final int quantidade )
-    {
-        Ação ação;
-        
-        for( int i = 0; i < ( quantidade / 5 ); i++ )
-        {
-            ação = new Ação( 2.2 + Biblioteca.gerarNumeroAleatorio(),
-                10 + Biblioteca.gerarNumeroAleatorio(), "Tabajara SA"
-                    + Biblioteca.gerarNumeroAleatorio() );
-            conta.getInventario().adicionarAoInventario( ação );
-            
-            ação = new Ação( 22.2 + Biblioteca.gerarNumeroAleatorio(),
-                100 + Biblioteca.gerarNumeroAleatorio(), "Tabajara SO"
-                    + Biblioteca.gerarNumeroAleatorio() );
-            conta.getInventario().adicionarAoInventario( ação );
-            
-            ação = new Ação( 200.2 + Biblioteca.gerarNumeroAleatorio(),
-                1000 + Biblioteca.gerarNumeroAleatorio(), "Tabajara SP"
-                    + Biblioteca.gerarNumeroAleatorio() );
-            conta.getInventario().adicionarAoInventario( ação );
-            
-            ação = new Ação( 2000.2 + Biblioteca.gerarNumeroAleatorio(),
-                10000 + Biblioteca.gerarNumeroAleatorio(), "Tabajara ST"
-                    + Biblioteca.gerarNumeroAleatorio() );
-            conta.getInventario().adicionarAoInventario( ação );
-            
-            ação = new Ação( 200006.2 + Biblioteca.gerarNumeroAleatorio(),
-                10000 + Biblioteca.gerarNumeroAleatorio(), "Tabajara SS"
-                    + Biblioteca.gerarNumeroAleatorio() );
-            conta.getInventario().adicionarAoInventario( ação );
-        }
-    }
-    
-    /**
      * Retorna a única instancia existe do MotorDoHomebroker.
      * 
      * @return INSTANCE a única instancia existe da JanelaPrincipal.
      */
-    public static MotorDoHomebroker getInstance()
+    public static MotorDoHomebroker getInstância()
     {
         return MotorDoHomebroker.INSTÂNCIA_DO_MOTOR;
     }
