92bbf158c05b3f00029e74ed85093f4a75f84626
evandrocoan <evandrocoan@hotmail.com>
2015-05-10 18:54:27 -0300

    Implementado os testes das operações básicas de um grafo.

--- Principal.java
+++ Principal.java
@@ -1,6 +1,10 @@
 import java.util.Enumeration;
 import java.util.Hashtable;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 /**
  * Testa o Grafo. Sobre esta estrutura, implementar algum algoritmo (de busca,
  * de solução de algum problema, etc) que a teste.
@@ -9,11 +13,8 @@ import java.util.Hashtable;
  */
 public class Principal
 {
-    private static int falharam = 0;
-    private static int total = 0;
-    
     /**
-     * Retorna uma representação em string da enumeração. Está enumeração deve
+     * Retorna uma representação em string da enumeração. Esta enumeração deve
      * ter um método toString que gere valores que possam significativamente
      * representar a string.
      * 
@@ -32,40 +33,25 @@ public class Principal
         return string;
     }
     
-    private static void finalizarTestes()
-    {
-        System.out.println( Principal.falharam + " testes de "
-                + Principal.total + " falharam!" );
-        System.out.println( ( Principal.total - Principal.falharam )
-                + " testes de " + Principal.total + " foram bem sucedidos!" );
-    }
-    
-    /**
-     * @param args os argumentos do chamada.
-     */
     public static void main( final String[] args )
     {
         Principal.testeGenéricoHashtable();
-        
-        try
-        {
-            Principal.testeDeGrau();
-            
-        } catch( final ExeçãoElementoNãoEncontrado exeção )
-        {
-            // TODO
-        }
-        
-        Principal.finalizarTestes();
         Principal.testeDaBase();
         
-        org.junit.runner.JUnitCore.main( "GrafoTest" );
+        // org.junit.runner.JUnitCore.main( "GrafoTest" );
+        final Result result = JUnitCore.runClasses( GrafoTest.class );
+        for( final Failure failure: result.getFailures() )
+        {
+            System.out.println( "Teste falhou: " + failure.getDescription() );
+        }
+        System.out.println( "\nTodos os testes funcinaram? "
+            + ( result.wasSuccessful()? "Sim!" : "NÃO!!!!!!!!!!!!!!!!!!!" ) );
     }
     
     private static void testeDaBase()
     {
         final Hashtable< Object, Hashtable< Object, Object >> vértices =
-                new Hashtable<>();
+            new Hashtable<>();
         
         // cria dois vértices
         final String vértice1 = new String( "Este é o vértice 1" );
@@ -81,9 +67,9 @@ public class Principal
         
         // pega a Hashtable de arestas do vértice
         final Hashtable< Object, Object > arestasDoVértice1 =
-                vértices.get( vértice1 );
+            vértices.get( vértice1 );
         final Hashtable< Object, Object > arestasDoVértice2 =
-                vértices.get( vértice2 );
+            vértices.get( vértice2 );
         
         // conecta o vértice1 com o vértice2
         arestasDoVértice1.put( chaveDoVértice2, vértice2 );
@@ -91,59 +77,12 @@ public class Principal
         
         // imprime o resultado
         System.out.println( Principal.enumeraçãoToString( arestasDoVértice1
-                .elements() ) + "(deve ter impresso o vértice 2)" );
+            .elements() )
+            + "(deve ter impresso o vértice 2)" );
         
         System.out.println( Principal.enumeraçãoToString( arestasDoVértice2
-                .elements() ) + "(deve ter impresso o vértice 1)" );
-    }
-    
-    private static void testeDeGrau() throws ExeçãoElementoNãoEncontrado
-    {
-        final String[] nomes =
-                { "Brazil", "United States", "China", "Hong Kong" };
-        final Grafo grafo = new Grafo();
-        boolean sucesso = false;
-        
-        try
-        {
-            grafo.adicionarVértice( nomes );
-            grafo.adicionarVértice( "Franca" );
-            grafo.conectarVértices( "Franca", nomes );
-            grafo.conectarVértices( "Brazil", nomes );
-            
-        } catch( final ExeçãoVérticeJáExistente e1 )
-        {
-            e1.printStackTrace();
-        }
-        Principal.falharam++;
-        Principal.total++;
-        
-        try
-        {
-            if( ( grafo.grauDoVértice( "Franca" ) == 4 )
-                    & ( grafo.grauDoVértice( "Brazil" ) == 6 ) )
-            {
-                sucesso = true;
-                Principal.falharam--;
-            } else
-            {
-                System.out.println( "Grau do vértice Franca é: "
-                        + grafo.grauDoVértice( "Franca" ) );
-                System.out.println( "Mas deveria ser: 4" );
-                System.out.println();
-                
-                System.out.println( "Grau do vértice Brasil é: "
-                        + grafo.grauDoVértice( "Brazil" ) );
-                System.out.println( "Mas deveria ser: 6" );
-                System.out.println();
-                
-                System.out.println( grafo.toString() );
-            }
-        } catch( final ExeçãoElementoNãoEncontrado e )
-        {
-            e.printStackTrace();
-        }
-        System.out.println( "Teste de grau: " + ( sucesso? "OK!" : "Falhou!" ) );
+            .elements() )
+            + "(deve ter impresso o vértice 1)\n" );
     }
     
     private static void testeGenéricoHashtable()
