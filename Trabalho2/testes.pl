7bb9ec0d2655f0669d688ffda45d0e854a298205
evandrocoan <evandrocoan@hotmail.com>
2015-05-18 16:10:04 -0300

    Acrescentado arquivos com exemplos de códigos.

---
+++ Trabalho2/testes.pl
@@ -0,0 +1,61 @@
+/* Para funcionar, dever selecionar o working directory para uma pasta acima
+ *   da pasta Trabalho2 executando o arquivo de configuração.
+ *
+ * Muda para o diretório de trabalho padrão.
+ *
+ * irParaDiretorioPadrao :- 
+ *	
+ *	working_directory(_, 'D:/Evandro/Archives/Dropbox/Aplicativos/
+ *                     SoftwareVersioning/2015-1_ParadigmasDeProgramacao').   
+ *
+ * :-irParaDiretorioPadrao.
+ * */
+importarTrabalho1ParteB :- ['Trabalho1/trabalho1ParteB.pl'].
+:-importarTrabalho1ParteB. 
+
+lerArquivo :- 
+	open('Trabalho1/bancoDeDados.pl', read, X),
+	
+	% salva a entrada padrão, para restaurar mais tarde.
+	current_input(Stream),
+	set_input(X),
+	
+	write(X),
+	
+	set_input(Stream),
+	close(X).
+
+% open(’arq.saida’, write, X).
+
+
+main :-
+    open('Trabalho1/bancoDeDados.pl', read, Str),
+    read(Str, X),
+    write(X).
+
+get_userinfo(personal(Name, Gender, Age, Attr)) :-
+      write('Enter name: '),
+      readln([Name|_]),
+      write('Enter gender: '),
+      readln([Gender|_]),
+      write('Enter age: '),
+      readln([Age|_]),
+      write('Enter attr: '),
+      readln([Attr|_]).
+
+record_userinfo(Person) :-
+      append('Trabalho1/bancoDeDados.pl'),
+      write(Person), nl,
+      told.
+
+teste :- 
+	consult('Trabalho1/bancoDeDados.pl'),
+	assert(informacoesProfissionais('asdfkasjdfl','asldkfjsdlafkj')), 
+	
+	/* Lista todas as clausulas informacoesProfissionais e grava no arquivo */
+	tell('Trabalho1/bancoDeDados.pl'),   
+	listing(informacoesAcademicas),
+	listing(informacoesPessoais), 
+	listing(informacoesProfissionais),
+	told.
+
