0145bf8b7f884ea97f587049d8dc4ad4ac2e3259
evandrocoan <evandrocoan@hotmail.com>
2015-05-25 22:20:07 -0300

    Iniciado a parte B do trabalho 2.

---
+++ Trabalho2/trabalho2ParteB.pl
@@ -0,0 +1,20 @@
+/* Para funcionar, dever selecionar o working directory para uma pasta acima
+ *   da pasta Trabalho2 executando o arquivo de configuração.
+ *
+ * Muda para o diretório de trabalho padrão.
+ *
+ * irParaDiretorioPadrao :- 
+ *  
+ *  working_directory(_, 'D:/Evandro/Archives/Dropbox/Aplicativos/
+ *                     SoftwareVersioning/2015-1_ParadigmasDeProgramacao').   
+ *
+ * :-irParaDiretorioPadrao.
+ * */
+importarTrabalho2ParteA :- ['Trabalho2/trabalho2ParteA.pl'].
+:-importarTrabalho2ParteA.
+
+
+
+
+
+
