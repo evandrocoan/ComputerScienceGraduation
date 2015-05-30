14695fc6a15c9b5bb5111f1a2169732aa8057d1b
evandrocoan <evandrocoan@hotmail.com>
2015-05-30 19:40:43 -0300

    Corrigido bug onde o programa não iniciava em modo de teste. Transferidos os métodos de utilitário de contas para o motor do homebroker.

--- homebroker/homebroker/testes/DriverClass.java
+++ homebroker/homebroker/testes/DriverClass.java
@@ -7,25 +7,25 @@ import homebroker.Homebroker;
 
 /**
  * Esta classe inicia o programa principal em mode de teste.
- * 
+ *
  * @author Professional
  */
 public final class DriverClass
 {
-    /**
-     * Torna esse uma classe de utilizades, impedindo sua instânciação.
-     */
-    private DriverClass()
-    {
-    }
-    
-    /**
-     * Método principal que inicia a execução dos testes
-     * 
-     * @param args os argumentos passados por linha de comando
-     */
-    public static void main( final String... args )
-    {
-        Homebroker.main( "teste" );
-    }
+   /**
+    * Torna esse uma classe de utilidades, impedindo sua instanciação.
+    */
+   private DriverClass()
+   {
+   }
+   
+   /**
+    * Método principal que inicia a execução dos testes
+    *
+    * @param args os argumentos passados por linha de comando
+    */
+   public static void main( final String... args )
+   {
+      Homebroker.main( "teste" );
+   }
 }
