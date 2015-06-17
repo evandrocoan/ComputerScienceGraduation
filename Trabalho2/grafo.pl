228b1cceb90c3878bd54471e7a5256461586a85e
evandrocoan <evandrocoan@hotmail.com>
2015-06-16 21:18:16 -0300

    Completado a criação do grafo de referências.

--- Trabalho2/grafo.pl
+++ Trabalho2/grafo.pl
@@ -1,9 +1,3 @@
 :- dynamic aresta/2.
 
-aresta(a, b).
-
 :- dynamic vertice/1.
-
-vertice(a).
-vertice(b).
-
--- Trabalho2/trabalho2ParteB.pl
+++ Trabalho2/trabalho2ParteB.pl
@@ -14,27 +14,57 @@ importarTrabalho2ParteA :- ['Trabalho2/trabalho2ParteA.pl'].
 :-importarTrabalho2ParteA.
 
 
-/* Escreva uma regra para a criação apropriada de um grafo (escolha uma das 
- *   formas vistas em aula) de todas as pessoas (nós ou vértices) e suas 
- *   respectivas referências (arestas). 
+/* Escreva uma regra para a criação apropriada de um grafo (escolha uma das formas vistas em 
+ *   aula) de todas as pessoas (nós ou vértices) e suas respectivas referências (arestas). E 
+ *   faça a persistência deste grafo em um arquivo '.pl'.
  * */
-%construirGrafo(Grafo) :-
+construirGrafo :-
 	
-	/* Cria um grafo com as referências de uma pessoa.
-	 * 
-	construirGrafo_Privado(Nome, Grafo):-
-		quaisColegasDe(Nome, Referencias),*/
+    privado_CarregaListaNomes(Lista),
+    privado_ConstruirGrafo_Recursao(Lista),
+    !.
+
+	/* Recebe uma Lista de todos os nomes que existem no Banco de Dados e adiciona todas as pessoas 
+	 *   com suas referências ao Grafo.
+	 * */
+	privado_ConstruirGrafo_Recursao([]).
+	    
+	privado_ConstruirGrafo_Recursao(Lista) :-
+	    dividirLista(Lista, 1, ElementoTemporario, RestoLista),
+	    primeiro(ElementoTemporario, PessoaAtual),
+	    
+	    % Adiciona as Referencias da pessoa PessoaAtual no grafo.
+	    quaisColegasDe(PessoaAtual, Referencias), 
+	    adicionarVertice(PessoaAtual), 
+	    privado_AdicionarReferencias_Recursao(PessoaAtual, Referencias), 
+	
+	    privado_ConstruirGrafo_Recursao(RestoLista).
+
+	/* Para uma dada Pessoa, adiciona todas as suas Referencias ao Grafo.
+	 * */
+	privado_AdicionarReferencias_Recursao(_, []).
+	
+	privado_AdicionarReferencias_Recursao(Pessoa, Lista) :-
+	    dividirLista(Lista, 1, ElementoTemporario, RestoLista),
+	    primeiro(ElementoTemporario, ReferenciaAtual),
+	    
+	    % Conecta a Pessoa a sua referência ReferenciaAtual no Grafo.
+        adicionarVertice(ReferenciaAtual), 
+	    conectar(Pessoa, ReferenciaAtual),
+	
+	    privado_AdicionarReferencias_Recursao(Pessoa, RestoLista).
 
 
 /* Recebe um Vértice e adiciona ele ao Grafo 'grafo.pl'. Caso o vertice já 
  *   exista não faz nada.
  * */
 adicionarVertice( Vertice ) :-
-    consult('Trabalho2/grafo.pl'), 
+    
     ( existeVertice( Vertice ) -> 
     
         write('O vertice: '), write(Vertice), write(' ja existe!')
 	;  
+	    consult('Trabalho2/grafo.pl'), 
         write('Escrevendo o vertice: '), write( Vertice ), write(' no arquivo.'),
         Predicado =.. [vertice, Vertice ],
         assert(Predicado),
@@ -47,43 +77,75 @@ adicionarVertice( Vertice ) :-
     ).
 
 
-/* Informa se os vértices estão conectados e torna o grafo não-orientado.
+/* Informa se os vértices estão conectados e torna este grafo não-orientado.
  * */
-estaoConectados(Vertice1, Vertice2) :- aresta(Vertice1, Vertice2).
-estaoConectados(Vertice1, Vertice2) :- aresta(Vertice2, Vertice1).
+estaoConectados(Vertice1, Vertice2) :- aresta(Vertice1, Vertice2), !.
+estaoConectados(Vertice1, Vertice2) :- aresta(Vertice2, Vertice1), !.
 
 
 /* Verifica se um dado Vértice já existe no grafo.
  * */
 existeVertice( Vertice ) :-
+	
+	% evita que o vértice seja inicializado
+	nonvar(Vertice), 
+	
+	consult('Trabalho2/grafo.pl'), 
+	vertice( Vertice ),
+	!.
+
+
+/* Verifica se um dado Aresta já existe no grafo e torna o grafo não orientado.
+ * */
+existeAresta( Vertice1, Vertice2 ) :-
+    
+    % evita que o vértice seja inicializado
+    nonvar(Vertice1), 
+    nonvar(Vertice2), 
+    
     consult('Trabalho2/grafo.pl'), 
-    vertice( Vertice ).
+    aresta( Vertice1, Vertice2 ),
+    !.
+
+existeAresta( Vertice1, Vertice2 ) :-
+    
+    % evita que o vértice seja inicializado
+    nonvar(Vertice1), 
+    nonvar(Vertice2), 
+    
+    consult('Trabalho2/grafo.pl'), 
+    aresta( Vertice2, Vertice1 ),
+    !.
 
 
 conectar(Vertice1, Vertice2):-
-    consult('Trabalho2/grafo.pl'), 
-    ( existeVertice( Vertice1 ) -> 
     
-        ( existeVertice( Vertice2 ) -> 
+    ( not( existeAresta( Vertice1, Vertice2 ) ) ->
+    
+	    ( existeVertice( Vertice1 ) -> 
 	    
-            write('Conectando os vertices: '), write( Vertice1 ), write(', '), write( Vertice2 ), 
-            write(' no arquivo.'),
-            Predicado =.. [aresta, Vertice1, Vertice2 ],
-            assert(Predicado),
-        
-            /* Lista todas as clausulas e grava no arquivo */
-            tell('Trabalho2/grafo.pl'), 
-            listing(aresta),
-            listing(vertice),
-            told
+	        ( existeVertice( Vertice2 ) -> 
+		    
+		        consult('Trabalho2/grafo.pl'), 
+	            write('Conectando os vertices: '), write( Vertice1 ), write(', '), 
+	            write( Vertice2 ), write(' no arquivo.'),
+	            Predicado =.. [aresta, Vertice1, Vertice2 ], 
+	            assert(Predicado),
+	        
+	            /* Lista todas as clausulas e grava no arquivo */
+	            tell('Trabalho2/grafo.pl'), 
+	            listing(aresta), 
+	            listing(vertice),
+	            told
+		    ;  
+	            write('O vertice2 nao existe!')
+		    ) 
 	    ;  
-            write('O vertice: '), write(Vertice2), write(' nao existe!')
-	    ) 
-    ;  
-        write('O vertice: '), write(Vertice1), write(' nao existe!')
+	        write('O vertice1 nao existe!')
+	    )
+    ;
+        write('A aresta: '), write( aresta( Vertice1, Vertice2 ) ), write(' ja existe!')
     ).
-
-
 
 
 
