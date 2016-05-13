5414683389a86b039859ef8fa651c16b92dd3600
evandrocoan <evandrocoan@hotmail.com>
2016-05-12 21:33:43 -0300

    Moved the "Algorithms.cpp"'s best fit algorithm to near the file begin.

--- SPs/MemoryManagement/Algorithms.cpp
+++ SPs/MemoryManagement/Algorithms.cpp
@@ -34,6 +34,97 @@ PartitionList* Algorithm::getPartitions()
 
 
 /**
+ * @see _BestFit::allocateMemory( unsigned int ) member class declaration.
+ */
+Partition* _BestFit::allocateMemory( unsigned int size ) 
+{
+    DEBUGGERLN( 2 + 32, "I AM ENTERING IN _BestFit::allocateMemory(1)" );
+    
+    Partition* novo;
+    auto part = this->getPartitions();
+    
+    DEBUGGERLN( 32, "( allocateMemory ) size: %d, part->size(): %d,", size, part->size() );
+    
+    if( part->size() == 0 )
+    {
+        novo = new Partition( 0, size - 1, false );
+        part->insert( novo );
+        
+        return novo;
+    }
+    
+    int  pos      = -1;
+    auto index    = part->begin();
+    int  partSize = 0 + ( *index )->getBeginAddress();
+    
+    DEBUGGERLN( 32, "( allocateMemory ) partSize: %d, index: %d, pos: %d,", partSize, index, pos );
+    
+    if( partSize >= size )
+    {
+        pos =0;
+    }
+    
+    int holeSize;
+    int end;
+    
+    int beg              = 0;
+    int forPartitionSize = part->size() - 1;
+    
+    for( int i = 0; i < forPartitionSize; ++i )
+    {
+        end = ( *index )->getEndAddress();
+        index++;
+        
+        beg      = ( *index )->getBeginAddress();
+        holeSize = ( beg - end ) + 1;
+        
+        DEBUGGERLN( 32, "( allocateMemory|for ) end: %d, index: %d, beg: %d, holeSize: %d,", end, index, beg, holeSize );
+        
+        if( holeSize >= size ) 
+        {
+            if( holeSize < partSize ) 
+            {
+                pos = beg;
+                partSize = holeSize;
+            }
+        }
+    }
+    
+    DEBUGGERLN( 32, "( allocateMemory ) end: %d, index: %d, beg: %d, holeSize: %d,", end, index, beg, holeSize );
+    
+    beg      = memoryManager->maxAddress;
+    end      = ( *index )->getBeginAddress();
+    holeSize = ( beg- end ) +1;
+    
+    DEBUGGERLN( 32, "( allocateMemory ) end: %d, index: %d, beg: %d, holeSize: %d,", end, index, beg, holeSize );
+    
+    if( holeSize >= size ) 
+    {
+        if( holeSize < partSize ) 
+        {
+            pos      = beg;
+            partSize = holeSize;
+        }
+    }
+    
+    DEBUGGERLN( 32, "( allocateMemory ) end: %d, index: %d, beg: %d, holeSize: %d,", end, index, beg, holeSize );
+    
+    if( pos==-1 )
+    {
+        return 0;
+    }
+    
+    novo = new Partition( pos, size - 1, false );
+    part->insert( novo );
+    
+    DEBUGGERLN( 32, "( allocateMemory ) size: %d, part->size(): %d,", size, part->size() );
+    
+    return novo;
+}
+
+
+
+/**
  * @see _FirstFit::allocateMemory( unsigned int ) member class declaration.
  */
 Partition* _FirstFit::allocateMemory( unsigned int size ) 
@@ -319,96 +410,6 @@ Partition* _WorstFit::allocateMemory( unsigned int size )
     return novo;
 }
 
-
-
-/**
- * @see _BestFit::allocateMemory( unsigned int ) member class declaration.
- */
-Partition* _BestFit::allocateMemory( unsigned int size ) 
-{
-    DEBUGGERLN( 2 + 32, "I AM ENTERING IN _BestFit::allocateMemory(1)" );
-    
-    Partition* novo;
-    auto part = this->getPartitions();
-    
-    DEBUGGERLN( 32, "( allocateMemory ) size: %d, part->size(): %d,", size, part->size() );
-    
-    if( part->size() == 0 )
-    {
-        novo = new Partition( 0, size - 1, false );
-        part->insert( novo );
-        
-        return novo;
-    }
-    
-    int  pos      = -1;
-    auto index    = part->begin();
-    int  partSize = 0 + ( *index )->getBeginAddress();
-    
-    DEBUGGERLN( 32, "( allocateMemory ) partSize: %d, index: %d, pos: %d,", partSize, index, pos );
-    
-    if( partSize >= size )
-    {
-        pos =0;
-    }
-    
-    int holeSize;
-    int end;
-    
-    int beg              = 0;
-    int forPartitionSize = part->size() - 1;
-    
-    for( int i = 0; i < forPartitionSize; ++i )
-    {
-        end = ( *index )->getEndAddress();
-        index++;
-        
-        beg      = ( *index )->getBeginAddress();
-        holeSize = ( beg - end ) + 1;
-        
-        DEBUGGERLN( 32, "( allocateMemory|for ) end: %d, index: %d, beg: %d, holeSize: %d,", end, index, beg, holeSize );
-        
-        if( holeSize >= size ) 
-        {
-            if( holeSize < partSize ) 
-            {
-                pos = beg;
-                partSize = holeSize;
-            }
-        }
-    }
-    
-    DEBUGGERLN( 32, "( allocateMemory ) end: %d, index: %d, beg: %d, holeSize: %d,", end, index, beg, holeSize );
-    
-    beg      = memoryManager->maxAddress;
-    end      = ( *index )->getBeginAddress();
-    holeSize = ( beg- end ) +1;
-    
-    DEBUGGERLN( 32, "( allocateMemory ) end: %d, index: %d, beg: %d, holeSize: %d,", end, index, beg, holeSize );
-    
-    if( holeSize >= size ) 
-    {
-        if( holeSize < partSize ) 
-        {
-            pos      = beg;
-            partSize = holeSize;
-        }
-    }
-    
-    DEBUGGERLN( 32, "( allocateMemory ) end: %d, index: %d, beg: %d, holeSize: %d,", end, index, beg, holeSize );
-    
-    if( pos==-1 )
-    {
-        return 0;
-    }
-    
-    novo = new Partition( pos, size - 1, false );
-    part->insert( novo );
-    
-    DEBUGGERLN( 32, "( allocateMemory ) size: %d, part->size(): %d,", size, part->size() );
-    
-    return novo;
-}
 
 
 
