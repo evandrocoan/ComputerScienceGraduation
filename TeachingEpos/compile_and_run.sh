d0866a827f0b87c6d388116d007f177f40acd9a5
evandrocoan <evandrocoan@hotmail.com>
2016-08-25 20:51:19 -0300

    Added a automatic remote computer compiler 'compile_remotely.sh'.

---
+++ TeachingEpos/compile_and_run.sh
@@ -0,0 +1,26 @@
+
+
+
+# Import the helper functions.
+. ./timer_calculator.sh
+
+
+# Get the application name
+export APPLICATION=$1
+make run;
+
+
+showTheElapsedSeconds "$0"
+
+
+
+
+
+
+
+
+
+
+
+
+
