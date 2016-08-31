8487ff1ecc4563f850d1976e243d41faf9e9c739
evandrocoan <evandrocoan@hotmail.com>
2016-08-31 11:19:48 -0300

    Added the application path handling.

--- TeachingEpos/compile_and_run.sh
+++ TeachingEpos/compile_and_run.sh
@@ -5,9 +5,19 @@
 . ./timer_calculator.sh
 
 
+# The application name or path to run
+APPLICATION_PATH=$1
+
+# Removed the '/app/blabla/' from the $1 argument name - https://regex101.com/r/rR0oM2/1
+APPLICATION_PATH=$(echo $APPLICATION_PATH | sed -r "s/((.+\/)+)//")
+
+# Removed the '.' from the $APPLICATION_PATH name.
+APPLICATION_NAME=$(echo $APPLICATION_PATH | cut -d'.' -f 1)
+
+
 # Get the application name
-export APPLICATION=$1
-make run;
+# qemu-system-i386 -smp 1 -m 262144k -nographic -no-reboot -fda  philosophers_dinner.img | tee philosophers_dinner.out
+make APPLICATION=$APPLICATION_NAME run
 
 
 showTheElapsedSeconds "$0"
