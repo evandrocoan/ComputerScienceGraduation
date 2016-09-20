71ab3d15118d3e884d39d19059d6997c44006708
evandrocoan <evandrocoan@hotmail.com>
2016-09-20 20:19:58 -0300

    Finished the retrosubstituion process on FirstTestChallenge fGaussTrid.m

--- 2016-2/Others/FirstTestChallenge/fGaussTrid.m
+++ 2016-2/Others/FirstTestChallenge/fGaussTrid.m
@@ -20,21 +20,21 @@ function x = fGaussTrid( n, t, r, d, b )
         printf( '\n\n\nStarting the %dº iteration...\n', i )
         
         # Navega/anda pelas linhas abaixo da diagonal principal
+        # 
+        # for M( 1, 1 ), mapping should return 2
+        # for M( 2, 2 ), mapping should return 2
+        # ...
         for j = i + 1 : i + 1
             
             j
             mapped_ji = mapping( j, i, line_count, column_count )
-            
-            # for M( 1, 1 ), mapping should return 2
-            # for M( 2, 2 ), mapping should return 2
-            # ...
             mapped_ii = mapping( i, i, line_count, column_count )
             
             dividendo = A( j, mapped_ji )
             divisor   = A( i, mapped_ii )
             aux       = dividendo / divisor
             
-            # A =                 M =      1  2  3  4  5  6 ... 51 
+            # A =                 M =      1  2  3  4  5  6 ... 51
             #    0   2  -1  -1           _________________________
             #   -1   3  -1   1        1 |  2 -1  0  0  0  0 ... -1
             #   -1   3  -1   1        2 | -1  3 -1  0  0  0 ...  1
@@ -66,10 +66,49 @@ function x = fGaussTrid( n, t, r, d, b )
         
     end
     
-    x = A( :, 4 );
+    printf( '\n' )
+    A
+    
+    x = retro_substitution( A, line_count, column_count );
+    printf( '\n' )
     
 end
 
+function solution = retro_substitution( A, line_count, column_count )
+    
+    printf( '\n\n\n\n\n\nStarting the retro_substitution...\n\n' )
+    
+    % Calculates the first term, 4x=5 --> x = 5/4
+    mapped_line_column_count      = mapping( line_count, line_count, line_count, column_count )
+    A( line_count, column_count ) = A( line_count, column_count ) / A( line_count, mapped_line_column_count );
+    
+    % Calculate the remaining terms.
+    for i = line_count - 1 : -1: 1
+        
+        printf( '\n\n\nStarting the %dº iteration...\n', i )
+        sum = 0
+        
+        for j = i + 1: -1: i + 1
+            
+            j
+            mapped_ij = mapping( i, j, line_count, column_count )
+            
+            multiplicado  = A( i, mapped_ij    )
+            multiplicador = A( j, column_count )
+            sum           = sum + multiplicado * multiplicador
+            
+        end
+        
+        mapped_ii = mapping( i, i, line_count, column_count )
+        
+        A( i, column_count ) = ( ( -1 * sum ) + A( i, column_count ) ) / A( i, mapped_ii );
+        i                    = i - 1
+        
+    end
+    
+    solution = A( :, column_count );
+    
+end
 
 
 
