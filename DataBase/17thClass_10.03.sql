59bc66d0a6f7fa731ae50d772e3d22846accc5c0
evandrocoan <evandrocoan@hotmail.com>
2016-10-22 23:24:39 -0200

    Finished the DataBase/22thClass_10.20.sql exercises.

--- DataBase/17thClass_10.03.sql
+++ DataBase/17thClass_10.03.sql
@@ -108,10 +108,11 @@ CREATE TABLE cidade
 
 -- This cannot be dropped, as depends:
 DROP TABLE IF EXISTS federacao CASCADE;
+
 -- 
 -- ERROR:  cannot drop table cidade because other objects depend on it
--- DETAIL:  constraint fk_cidade_pais on table federacao depends on table cidade
--- HINT:  Use DROP ... CASCADE to drop the dependent objects too.
+-- DETAIL: constraint fk_cidade_pais on table federacao depends on table cidade
+-- HINT:   Use DROP ... CASCADE to drop the dependent objects too.
 
 CREATE TABLE federacao
 (
@@ -131,5 +132,151 @@ CREATE TABLE federacao
 
 -- Undo all operations, change this comments to effectevly implement the changes into the data base.
 -- Or comment everything and also the `BEGIN TRANSACTION;` at this script beginning.
+
 ROLLBACK TRANSACTION;
 -- COMMIT TRANSACTION;
+
+
+
+
+--Questão 1
+BEGIN TRANSACTION;
+
+/*
+CREATE TABLE pais (
+codigo integer not null primary key,
+nome varchar (100),
+populacao integer
+);
+*/
+
+CREATE TABLE pais (
+codigo integer not null,
+nome varchar (100),
+populacao integer,
+CONSTRAINT pk_pais PRIMARY KEY (codigo)
+);
+
+CREATE TABLE cidade (
+codigo integer not null,
+nome varchar (100),
+UF char (2),
+regiao varchar (40),
+codPais integer,
+CONSTRAINT pk_cidade PRIMARY KEY (codigo),
+CONSTRAINT fk_pais_cidade FOREIGN KEY (codPais) REFERENCES pais (codigo)
+);
+
+CREATE TABLE federacao (
+codigo integer not null,
+nome varchar (100) UNIQUE,
+codPais integer,
+CONSTRAINT pk_federacao PRIMARY KEY (codigo),
+CONSTRAINT fk_pais_federacao FOREIGN KEY (codPais) REFERENCES pais (codigo)
+);
+
+CREATE TABLE equipe (
+codigo integer not null,
+nome varchar (100),
+codFederacao integer,
+codCidade integer,
+CONSTRAINT pk_equipe PRIMARY KEY (codigo),
+CONSTRAINT fk_federacao_equipe FOREIGN KEY (codFederacao) REFERENCES federacao (codigo),
+CONSTRAINT fk_cidade_equipe FOREIGN KEY (codCidade) REFERENCES cidade (codigo)
+);
+
+CREATE TABLE jogo (
+codTimeA integer not null,
+codTimeB integer not null,
+dataJogo date not null CHECK (dataJogo >= current_date),
+codCidade integer,
+CONSTRAINT pk_jogo PRIMARY KEY (codTimeA, codTimeB, dataJogo),
+CONSTRAINT fk_equipeA_jogo FOREIGN KEY (codTimeA) REFERENCES equipe (codigo) ON DELETE CASCADE,
+CONSTRAINT fk_equipeB_jogo FOREIGN KEY (codTimeB) REFERENCES equipe (codigo) ON DELETE CASCADE,
+CONSTRAINT fk_cidade_jogo FOREIGN KEY (codCidade) REFERENCES cidade (codigo)
+);
+
+CREATE TABLE resultado (
+codTimeA integer not null,
+codTimeB integer not null,
+dataJogo date not null,
+vencedor varchar (20) check (vencedor = 'Mandante' OR vencedor = 'Visitante' OR vencedor = 'Empate' OR vencedor = 'Sem resultado') DEFAULT 'Sem resultado',
+CONSTRAINT pk_resultado PRIMARY KEY (codTimeA, codTimeB, dataJogo),
+CONSTRAINT fk_jogo_resultado FOREIGN KEY (codTimeA, codTimeB, dataJogo) REFERENCES jogo (codTimeA, codTimeB, dataJogo)
+);
+
+-- ROLLBACK TRANSACTION;
+COMMIT TRANSACTION;
+
+--Questão 2
+BEGIN TRANSACTION;
+
+ALTER TABLE jogo
+ADD vencedor varchar (20) check (vencedor = 'Mandante' OR vencedor = 'Visitante' OR vencedor = 'Empate' OR vencedor = 'Sem resultado') DEFAULT 'Sem resultado';
+
+DROP TABLE resultado;
+
+ALTER TABLE cidade
+ALTER UF SET DEFAULT '-',
+DROP regiao,
+ADD regiao_nacional varchar (40);
+
+-- ROLLBACK TRANSACTION;
+COMMIT TRANSACTION;
+
+--Questão 3
+BEGIN TRANSACTION;
+INSERT INTO pais (codigo, nome, populacao) VALUES (1, 'Brasil', 200);
+INSERT INTO pais (codigo, nome, populacao) VALUES (2, 'Argentina', 41);
+
+INSERT INTO cidade (codigo, nome, UF, regiao_nacional, codPais) VALUES (1, 'Florianópolis', 'SC', 'Sul', 1);
+INSERT INTO cidade (codigo, nome, UF, regiao_nacional, codPais) VALUES (2, 'Porto Alegre', 'RS', 'Sul', 1);
+INSERT INTO cidade (codigo, nome, codPais) VALUES (3, 'Buenos Aires', 2);
+
+INSERT INTO federacao (codigo, nome, codPais) VALUES (1, 'CBF', 1);
+
+INSERT INTO equipe (codigo, nome, codFederacao, codCidade) VALUES (1, 'Avai', 1, 1);
+INSERT INTO equipe (codigo, nome, codFederacao, codCidade) VALUES (2, 'Figueirense', 1, 1);
+INSERT INTO equipe (codigo, nome, codFederacao, codCidade) VALUES (3, 'Gremio', 1, 2);
+INSERT INTO equipe (codigo, nome, codCidade) VALUES (4, 'Boca Juniors', 3);
+
+INSERT INTO jogo (codTimeA, codTimeB, dataJogo, codCidade) VALUES (1, 2, current_date, 1);
+INSERT INTO jogo (codTimeA, codTimeB, dataJogo, codCidade) VALUES (3, 4, current_date + interval '1' day, 2);
+
+--Se tabela resultado ainda existisse
+/*
+INSERT INTO resultado (codTimeA, codTimeB, dataJogo, vencedor) VALUES (1, 2, current_date, 'Mandante');
+INSERT INTO resultado (codTimeA, codTimeB, dataJogo) VALUES (3, 4, current_date + interval '1' day);
+*/
+
+UPDATE jogo
+SET vencedor = 'Mandante'
+WHERE codTimeA = 1 AND codTimeB = 2 and dataJogo = current_date;
+
+UPDATE jogo
+SET vencedor = 'Sem resultado' --valor default
+WHERE codTimeA = 1 AND codTimeB = 2 and dataJogo = current_date + interval '1' day;
+
+--ROLLBACK TRANSACTION;
+COMMIT TRANSACTION;
+
+--Questão 4
+BEGIN TRANSACTION;
+
+UPDATE equipe
+SET codCidade = 2
+WHERE codigo = 4;
+
+DELETE FROM jogo
+WHERE codTimeA = 1;
+DELETE FROM equipe
+WHERE codigo = 1;
+
+DELETE FROM equipe
+WHERE codigo = 1;
+
+--ROLLBACK TRANSACTION;
+COMMIT TRANSACTION;
+
+
+
---
+++ DataBase/22thClass_10.20.sql
@@ -0,0 +1,395 @@
+﻿
+/*
+
+Departamento (codDepto, nome)
+Empregado (codemp, nome, endereco, dataNasc, salario, codDepto)
+    codDepto referencia departamento (codDepto)
+
+Projeto (codproj, titulo)
+Trabalhaem(codEmp, codProj, horasTrab)
+    codEmp referencia Empregado (codEmp)
+    codProj referencia Projeto (codProj)
+
+*/
+
+-- below code assumes that the name of your schema is public
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+-- For PostgreSQL 9.3 or greater, you may also need to restore the default grants.
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: Departamento (codDepto, nome)'
+\echo 'INFO: Empregado (codemp, nome, endereco, dataNasc, salario, codDepto)'
+\echo 'INFO:     codDepto referencia departamento (codDepto)'
+\echo 'INFO:'
+\echo 'INFO: Projeto (codproj, titulo)'
+\echo 'INFO: Trabalhaem(codEmp, codProj, horasTrab)'
+\echo 'INFO:     codEmp referencia Empregado (codEmp)'
+\echo 'INFO:     codProj referencia Projeto (codProj)'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: I. Criar as tabelas com CREATE TABLE.'
+\echo 'INFO:'
+
+\echo 'INFO: Departamento (codDepto, nome)'
+
+CREATE TABLE Departamento
+(
+    codDepto integer, -- PRIMARY KEY, -- Second method of creation
+    nome     varchar,
+
+    -- CONSTRAINT + nome_da_constraint
+    CONSTRAINT pk_departamento PRIMARY KEY(codDepto)
+);
+
+\echo 'INFO: Empregado (codemp, nome, endereco, dataNasc, salario, codDepto)'
+\echo 'INFO:     codDepto referencia departamento (codDepto)'
+
+CREATE TABLE Empregado
+(
+    codemp   integer, -- PRIMARY KEY, -- Second method of creation
+    nome     varchar,
+    endereco varchar,
+    fone     integer,
+    dataNasc date,
+    salario  integer,
+    codDepto integer,
+
+    -- CONSTRAINT + nome_da_constraint
+    CONSTRAINT pk_empregado PRIMARY KEY(codemp),
+
+    CONSTRAINT fk_empregado FOREIGN KEY(codDepto)
+            REFERENCES Departamento(codDepto) ON DELETE CASCADE
+);
+
+\echo 'INFO: Projeto (codproj, titulo)'
+
+CREATE TABLE Projeto
+(
+    codproj integer, -- PRIMARY KEY, -- Second method of creation
+    titulo  varchar,
+
+    -- CONSTRAINT + nome_da_constraint
+    CONSTRAINT pk_projeto PRIMARY KEY(codproj)
+);
+
+\echo 'INFO: Trabalhaem(codEmp, codProj, horasTrab)'
+\echo 'INFO:     codEmp referencia Empregado (codEmp)'
+\echo 'INFO:     codProj referencia Projeto (codProj)'
+
+CREATE TABLE Trabalhaem
+(
+    codEmp    integer, -- PRIMARY KEY, -- Second method of creation
+    codProj   integer,
+    horasTrab integer,
+
+    -- CONSTRAINT + nome_da_constraint
+    CONSTRAINT pk_trabalhaem PRIMARY KEY(codEmp,codProj),
+
+    CONSTRAINT fk_empregado FOREIGN KEY(codEmp)
+            REFERENCES Empregado(codemp) ON DELETE CASCADE,
+
+    CONSTRAINT fk_projeto FOREIGN KEY(codProj)
+            REFERENCES Projeto(codproj) ON DELETE CASCADE
+);
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: II. Inserir dados com INSERT (deve haver dados para ter resultados para todas as consultas)'
+\echo 'INFO:'
+
+\echo 'INFO: Departamento (codDepto, nome)'
+
+insert into Departamento VALUES( 1, 'Dep: Ticular' );
+insert into Departamento VALUES( 2, 'Dep: ParUni'  );
+insert into Departamento VALUES( 3, 'Pesquisa'     );
+insert into Departamento VALUES( 4, 'Dep: Tarar'   );
+
+\echo 'INFO: Empregado (codemp, nome, endereco, dataNasc, salario, codDepto)'
+\echo 'INFO:     codDepto referencia departamento (codDepto)'
+
+INSERT INTO Empregado( codemp, nome, endereco, dataNasc, salario, codDepto ) VALUES( 1, 'Maria Aparecida' , 'Rua Ortências', '1970-01-04', 20000, 1 );
+INSERT INTO Empregado( codemp, nome, endereco, dataNasc, salario, codDepto ) VALUES( 2, 'Carolina Pereira', 'Av. das Rosas', '1979-11-07', 10000, 2 );
+INSERT INTO Empregado( codemp, nome, endereco, dataNasc, salario, codDepto ) VALUES( 3, 'Antonio Carlos'  , 'Rua Caligari' , '1980-11-04', 50000, 3 );
+INSERT INTO Empregado( codemp, nome, endereco, dataNasc, salario, codDepto ) VALUES( 4, 'Mariana Faria'   , 'Rua Luiz XI'  , '1961-10-14', 60000, 4 );
+INSERT INTO Empregado( codemp, nome, endereco, dataNasc, salario, codDepto ) VALUES( 5, 'Faria Antonio'   , 'Rua XI'       , '1991-10-14', 1000 , 4 );
+
+\echo 'INFO: Projeto (codproj, titulo)'
+
+insert into Projeto VALUES( 1, 'Proj: Particular' );
+insert into Projeto VALUES( 2, 'Proj: Uni'        );
+insert into Projeto VALUES( 3, 'Transmogrifador'  );
+insert into Projeto VALUES( 4, 'Tarar'            );
+
+\echo 'INFO: Trabalhaem(codEmp, codProj, horasTrab)'
+\echo 'INFO:     codEmp referencia Empregado (codEmp)'
+\echo 'INFO:     codProj referencia Projeto (codProj)'
+
+INSERT INTO Trabalhaem VALUES( 1, 2, 5   );
+INSERT INTO Trabalhaem VALUES( 2, 3, 101 );
+INSERT INTO Trabalhaem VALUES( 3, 1, 110 );
+INSERT INTO Trabalhaem VALUES( 4, 2, 12  );
+INSERT INTO Trabalhaem VALUES( 1, 3, 120 );
+INSERT INTO Trabalhaem VALUES( 2, 4, 120 );
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: III. Responder as questões abaixo com consultas SQL.'
+\echo 'INFO:'
+
+\echo 'INFO: 1. Alterar o salário do empregado de código 3 para 28000'
+\echo 'INFO:'
+
+UPDATE empregado
+SET salario = 28000
+WHERE empregado.codEmp = 3;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 2 - Obter nomes de empregados com salario > 30000.'
+\echo 'INFO:'
+
+SELECT empregado.nome, empregado.salario
+FROM empregado
+WHERE empregado.salario > 30000;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 3. Obter nomes de empregados que trabalham no projeto \'Transmogrifador\'. '
+\echo 'INFO:'
+
+SELECT empregado.nome, projeto.titulo
+FROM empregado JOIN Trabalhaem ON empregado.codemp   = Trabalhaem.codEmp
+               JOIN Projeto    ON Trabalhaem.codProj = Projeto.codproj
+WHERE Projeto.titulo = 'Transmogrifador';
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 4. Obter nomes e endereços de todos os empregados que trabalham no departamento '
+\echo 'INFO: de \'Pesquisa\'. '
+\echo 'INFO:'
+
+SELECT empregado.nome, empregado.endereco, Departamento.nome
+FROM empregado JOIN Departamento ON empregado.codDepto = Departamento.codDepto
+WHERE departamento.nome = 'Pesquisa';
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 5. Obter nomes de empregados que começam com a letra \'A\'.'
+\echo 'INFO:'
+
+SELECT empregado.nome
+FROM empregado
+WHERE empregado.nome LIKE 'A%';
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 6. Obter os nomes e datas de nascimento dos empregados que fazem aniversário no mês de'
+\echo 'INFO: outubro. Considere a data como tipo char(10).'
+\echo 'INFO:'
+
+SELECT empregado.nome, empregado.dataNasc
+FROM empregado
+WHERE EXTRACT( month FROM empregado.dataNasc ) = 10;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 7. Obter os nomes dos empregados nascidos entre as datas 1950-01-01 e 1970-01-01.'
+\echo 'INFO:'
+
+SELECT empregado.nome, empregado.dataNasc
+FROM empregado
+WHERE empregado.dataNasc > '1950-01-01'
+  AND empregado.dataNasc < '1970-01-01';
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 8. Listar os títulos de projetos em ordem alfabética.'
+\echo 'INFO:'
+
+SELECT Projeto.titulo
+FROM Projeto
+ORDER BY Projeto.titulo ASC;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 9. Listar nomes e horas trabalhadas por empregados no projeto de código 3, em ordem'
+\echo 'INFO: decrescente de horas trabalhadas.'
+\echo 'INFO:'
+
+SELECT empregado.nome, Trabalhaem.horasTrab
+FROM empregado JOIN Trabalhaem ON empregado.codemp   = Trabalhaem.codEmp
+               JOIN Projeto    ON Trabalhaem.codProj = Projeto.codproj
+WHERE Projeto.codproj = 3
+ORDER BY Trabalhaem.horasTrab DESC;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 10. Obter códigos de empregados que trabalham mais de 10 horas em algum projeto.'
+\echo 'INFO: O resultado da consulta não deve ter repetições de códigos de empregados.'
+\echo 'INFO:'
+
+SELECT DISTINCT ON( empregado.codemp )
+        empregado.codemp, Trabalhaem.horasTrab
+FROM empregado JOIN Trabalhaem ON empregado.codemp = Trabalhaem.codEmp
+WHERE Trabalhaem.horasTrab > 20;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 11. Obter a quantidade de empregados pertencentes ao departamento 4.'
+\echo 'INFO:'
+
+SELECT COUNT( empregado.codDepto )
+FROM empregado
+WHERE empregado.codDepto = 4;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 12. Obter, a partir da tabela trabalhaEm, os números mínimo, máximo e médio de'
+\echo 'INFO: horas trabalhadas por empregados em cada projeto. O resultado deve possuir 4'
+\echo 'INFO: colunas nomeadas: projeto, minimo, maximo e media.'
+\echo 'INFO:'
+
+SELECT Projeto.titulo AS projeto, MIN( Trabalhaem.horasTrab ) AS minimo, MAX( Trabalhaem.horasTrab ) AS maximo, AVG( Trabalhaem.horasTrab ) AS media
+FROM Projeto JOIN Trabalhaem ON Projeto.codproj = Trabalhaem.codProj
+GROUP BY 1;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 13. Obter os códigos de projetos cuja média de horas trabalhadas seja maior que 20.'
+\echo 'INFO:'
+
+SELECT Projeto.codproj, AVG( Trabalhaem.horasTrab )
+FROM Projeto JOIN Trabalhaem ON Projeto.codproj = Trabalhaem.codProj
+GROUP BY 1
+HAVING AVG( Trabalhaem.horasTrab ) > 20;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 14. Obter os nomes de projetos correspondentes à consulta anterior. Usar a consulta'
+\echo 'INFO: anterior como uma consulta aninhada à nova consulta.'
+\echo 'INFO:'
+
+SELECT Projeto.titulo
+FROM ( SELECT Projeto.codproj, AVG( Trabalhaem.horasTrab )
+        FROM Projeto JOIN Trabalhaem ON Projeto.codproj = Trabalhaem.codProj
+        GROUP BY 1
+        HAVING AVG( Trabalhaem.horasTrab ) > 20
+     ) resultTable JOIN Projeto ON resultTable.codproj = Projeto.codproj;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 15. Obter o nome dos empregados e quantidade de projeto em que cada um deles'
+\echo 'INFO: já trabalhou. Empregados que não trabalharam em projetos também devem'
+\echo 'INFO: aparecer no resultado.'
+\echo 'INFO:'
+
+SELECT Empregado.nome, COUNT( Projeto.codProj ) AS projetos
+FROM Empregado LEFT OUTER JOIN Trabalhaem ON Empregado.codemp   = Trabalhaem.codEmp
+               LEFT OUTER JOIN Projeto    ON Trabalhaem.codProj = Projeto.codproj
+GROUP BY 1;
+
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO:'
+\echo 'INFO: 16. Obter o nome do projeto que mais teve empregados trabalhando.'
+\echo 'INFO:'
+
+WITH ContagemTable AS
+(
+    SELECT Projeto.codproj, Projeto.titulo, COUNT( Trabalhaem.codEmp )
+    FROM Empregado JOIN Trabalhaem ON Empregado.codemp   = Trabalhaem.codEmp
+                   JOIN Projeto    ON Trabalhaem.codproj = Projeto.codproj
+    GROUP BY 1
+)
+
+SELECT ContagemTable.titulo
+FROM ContagemTable
+WHERE ContagemTable.count = 
+    (
+        SELECT MAX( ContagemTable.count )
+        FROM ContagemTable JOIN Projeto ON ContagemTable.codproj = Projeto.codproj
+    );
+
+
+
+
+
+
+
+
