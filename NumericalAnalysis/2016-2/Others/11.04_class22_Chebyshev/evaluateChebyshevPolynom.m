b359e8bcfeb8e59ec55acb45b346fa58fd86c3e3
evandrocoan <evandrocoan@hotmail.com>
2016-11-06 21:16:19 -0200

    Implemented the recursive remember for Chebyshev b's coefficients.

---
+++ NumericalAnalysis/2016-2/Others/11.04_class22_Chebyshev/evaluateChebyshevPolynom.m
@@ -0,0 +1,39 @@
+
+# 
+# Evaluate the Chebyshev Polynom
+# 
+# i = 1 : k
+# fChebyshev( i ) = b0*T0( t(i) ) + b1*T1( t(i) ) + b2*T2( t(i) ) + b3*T3( t(i) ) + ...
+#
+# yAproximado = coef_b( 1)*T0( tInterPontos ) ...
+#             + coef_b( 2)*T1( tInterPontos ) ...
+#             + coef_b( 3)*T2( tInterPontos ) ...
+#             + coef_b( 4)*T3( tInterPontos ) ...
+#             + coef_b( 5)*T4( tInterPontos ) ...
+#             + coef_b( 6)*T5( tInterPontos ) ...
+#             + coef_b( 7)*T6( tInterPontos ) ...
+#             + coef_b( 8)*T7( tInterPontos ) ...
+#             + coef_b( 9)*T8( tInterPontos ) ...
+#             + coef_b(10)*T9( tInterPontos )
+#
+# 
+# @param m, Chebyshev's series degree.
+# @param b, the `m` b's Chebyshev coefficients.
+# @param t, the [-1, 1] points to evaluate the Chebyshev Polynom.
+# 
+function value = evaluateChebyshevPolynom( m, b, t )
+    
+    value = 0;
+    
+    for i = 1 : m
+        
+        value = value + b( i )*evaluateChebyshevPolynomOfFirstKindAt( i - 1, t );
+        
+    end
+    
+end
+
+
+
+
+
