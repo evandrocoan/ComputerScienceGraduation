5b93046aa7b4f9137e559b98778cc2302f1f65af
evandrocoan <evandrocoan@hotmail.com>
2016-11-11 12:28:35 -0200

    Added the exercise 6.1e) required functions.

---
+++ NumericalAnalysis/2016-2/Others/Work3_PoliInterpolation/run_pade_test.m
@@ -0,0 +1,71 @@
+
+
+###################################################################################################
+###################################################################################################
+###################################################################################################
+# 4-aproximação racional de padé
+#
+# yPade(x) = (a(1) + a(2)x + a(3)x^2 + a(4)x^3)/(1 + b(1)x + b(2)*x^2)
+#
+# R32 = (a(1) + a(2)x + a(3)x^2 + a(4)x^3)/(1 + b(1)x + b(2)*x^2)
+#
+# Como determinar o m e n do método de Pade?
+# Não é como saber, somente experimentando valores e verificando o resultado/erro.
+#
+# A método de Pade é útil para funções assintóticas como 1/x.
+# Ele é o contrário da Serie de Chebyshev, que não funciona para funções assintóticas.
+#
+# Por exemplo, a função log( x ) é assintótica entre perto de 0 até 1. Mas podemos utilizar
+# Chebyshev no função log( x ) no intervalo [1, 2] ou mais, onde a função é suave/bem comportada.
+#
+function run_pade_test( n, m, a, b, targetFunction )
+
+    h = (b-a)/n;
+
+    x = a : h : b;
+    y = targetFunction( x );
+
+    grauDeMaclaurinParaPade = n + m;
+    coefMaclaurinParaPade   = calculateMaclaurinCoefficientsForSin( grauDeMaclaurinParaPade, a, b )
+
+    xInterPontos      = a : h/20 : b;
+    yInterPontosExato = targetFunction( xInterPontos );
+
+    # R32 = (a(1) + a(2)x + a(3)x^2 + a(4)x^3)/(1 + b(1)x + b(2)*x^2)
+    #
+    [ aPadeCoefficients, bPadeCoefficients ] = calculatePadeCoefficients( n, m, coefMaclaurinParaPade );
+    tInterPontos = ChebyshevDomainLinearTransformationIn( xInterPontos, a, b );
+
+    # For log( x ) in [1, 2] with n = 3 and m = 2
+    ap_correct_value = [ 0.4054651   0.4955194   0.0912933   0.0012346 ];
+    aPadeCoefficients;
+
+    # For log( x ) in [1, 2] with n = 3 and m = 2
+
+    bp_correct_value = [ 1.00000     0.40000     0.03333     0.00000   ];
+    bPadeCoefficients;
+
+    # yAproximado = fPnPorBriotRunifi( n, coefMaclaurin, tInterPontos )
+    # yAproximadoPorPade = polyval( aPadeCoefficients, tInterPontos ) / polyval( bPadeCoefficients, tInterPontos );
+    #
+    yAproximadoPorPade = fPnPorBriotRunifi( ...
+            n, aPadeCoefficients, tInterPontos ) ./ fPnPorBriotRunifi( ...
+            m, bPadeCoefficients, tInterPontos );
+
+    #
+    # O erro de Pade é como o erro de Maclaurin, ele é 0 no ponto 0 entre [-1,1], como o Maclaurin.
+    # O Maclaurin tem esse erro por que o Maclaurin é expandido em torno do ponto 0.
+    # Mas mesmo o Pade sendo feito em base do Maclaurin, ele melhora um pouco o erro de Maclaurin com
+    # seu polinômio interpolador.
+    #
+    erroDePade       = abs( yAproximadoPorPade .- yInterPontosExato );
+    erroMaximoDePade = max( erroDePade )
+
+    # plot( x, y, '*' )
+    plot( x, y, '*', xInterPontos, yInterPontosExato, 'g', xInterPontos, yAproximadoPorPade, 'b' )
+
+end
+
+
+
+
