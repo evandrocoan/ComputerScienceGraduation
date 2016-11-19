21d8d3fee83afe14e953b1e9add9edc0840ddfa8
evandrocoan <evandrocoan@hotmail.com>
2016-11-18 22:41:32 -0200

    Added the fPnPorHorner documentation and adjusted the BriotRufini.

--- NumericalAnalysis/2016-2/Others/10.21_class19_Vandermonde/fPnPorBriotRunifi.m
+++ NumericalAnalysis/2016-2/Others/10.21_class19_Vandermonde/fPnPorBriotRunifi.m
@@ -1,21 +1,29 @@
 
+#
+# DO NOT USE THIS `SHIT` NEVER EVER! Use `fPnPorHorner` instead of this SHIT.
+#
 function yp = fPnPorBriotRunifi( n, a, xp )
-    
+
+    # The input polynom must to be on the form: 0 + 5*x^1 + 0*x^2 - 20*x^3 + 0*x^4 +16*x^5
+    # Without any leading zeros after the last `x^n` coefficient.
+    #
     a      = fliplr( a );
     b( 1 ) = a( 1 );
-    
-    for k = 1 : length( xp )
-        
+
+    pointsToProcess = length( xp );
+
+    for k = 1 : pointsToProcess
+
         for i = 2 : n + 1
-            
+
             b( i ) = a( i ) + xp( k )*b( i - 1 );
-            
+
         end
-        
+
         yp( k ) = b( n + 1 );
-        
+
     end
-    
+
 end
 
 
--- NumericalAnalysis/2016-2/Others/10.21_class19_Vandermonde/fPnPorHorner.m
+++ NumericalAnalysis/2016-2/Others/10.21_class19_Vandermonde/fPnPorHorner.m
@@ -1,21 +1,33 @@
+
+#
+# Calculates a given polynom as a coefficients vector on the given point(s).
+#
+# n , the polynom degree
+# a , the polynom coefficients on the form: 0 + 5*x^1 + 0*x^2 - 20*x^3 + 0*x^4 +16*x^5
+# xp, the vector or a point to evaluate the polynom on, as `0.5`: 0 + 5*(0.5)^1 + 0*(0.5)^2 - 20*(0.5)^3 + 0*(0.5)^4 +16*(0.5)^5
+#
+# @return the polynom's value on the given point. If a point's vector is given, a polynom's value
+#         vector within the points calculated is returned.
+#
 function yp = fPnPorHorner( n, a, xp )
-    
-    
-    for k = 1 : length( xp )
-        
+
+    pointsToProcess = length( xp );
+
+    for k = 1 : pointsToProcess
+
         # Precisamos limpar auxiliar a cada iteração
         aux = a( n + 1 );
-        
+
         for i = n : -1 : 1
-            
+
             aux = a( i ) + xp( k )*aux;
-            
+
         end
-        
+
         yp( k ) = aux;
-        
+
     end
-    
+
 end
 
 
