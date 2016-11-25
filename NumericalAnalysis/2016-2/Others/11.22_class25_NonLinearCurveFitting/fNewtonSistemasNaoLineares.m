e1d31d87c40996d76cbcbd7a8fb5882170f5d8cd
evandrocoan <evandrocoan@hotmail.com>
2016-11-25 14:47:02 -0200

    Created functions vector for 11.22_class25_NonLinearCurveFitting.

--- NumericalAnalysis/2016-2/Others/11.22_class25_NonLinearCurveFitting/fNewtonSistemasNaoLineares.m
+++ NumericalAnalysis/2016-2/Others/11.22_class25_NonLinearCurveFitting/fNewtonSistemasNaoLineares.m
@@ -4,7 +4,7 @@
 # 1) Chutar um valor inicial real, não converge caso as raízes da solução sejam complexas.
 # 2) Mas chutar um valor initial complexo, converge caso as raízes sejam reais ou complexas.
 #
-function x = fNewtonSistemasNaoLineares( xi, f1, f2 )
+function x = fNewtonSistemasNaoLineares( xi, functions )
 
     % São três passos:
     % Calcular delta x (dx)
@@ -20,13 +20,13 @@ function x = fNewtonSistemasNaoLineares( xi, f1, f2 )
 
         contador = contador + 1;
 
-        A( 1, 1 ) = ( f1( [ xi(1) + dx( 1 ), xi(2)         ] ) - f1( xi ) ) / dx(1);
-        A( 1, 2 ) = ( f1( [ xi(1)          , xi(2) + dx(2) ] ) - f1( xi ) ) / dx(2);
-        A( 1, 3 ) = - f1( xi );
+        A( 1, 1 ) = ( functions(1).vector( [ xi(1) + dx( 1 ), xi(2)         ] ) - functions(1).vector( xi ) ) / dx(1);
+        A( 1, 2 ) = ( functions(1).vector( [ xi(1)          , xi(2) + dx(2) ] ) - functions(1).vector( xi ) ) / dx(2);
+        A( 1, 3 ) = - functions(1).vector( xi );
 
-        A( 2, 1 ) = ( f2( [ xi(1) + dx( 1 ), xi(2)         ] ) - f2( xi ) ) / dx(1);
-        A( 2, 2 ) = ( f2( [ xi(1)          , xi(2) + dx(2) ] ) - f2( xi ) ) / dx(2);
-        A( 2, 3 ) = - f2( xi );
+        A( 2, 1 ) = ( functions(2).vector( [ xi(1) + dx( 1 ), xi(2)         ] ) - functions(2).vector( xi ) ) / dx(1);
+        A( 2, 2 ) = ( functions(2).vector( [ xi(1)          , xi(2) + dx(2) ] ) - functions(2).vector( xi ) ) / dx(2);
+        A( 2, 3 ) = - functions(2).vector( xi );
 
         A;
         dx = fgauss( 2, A );
--- NumericalAnalysis/2016-2/Others/11.22_class25_NonLinearCurveFitting/sistemas.m
+++ NumericalAnalysis/2016-2/Others/11.22_class25_NonLinearCurveFitting/sistemas.m
@@ -20,7 +20,15 @@ aInicial = [ 0.5, 0.5 ]
 f1Problema1aInicial = f1Problema1( aInicial )
 f2Problema1aInicial = f2Problema1( aInicial )
 
-aSolucao = fNewtonSistemasNaoLineares( aInicial, @f1Problema1, @f2Problema1 )
+functions(1).vector = @f1Problema1;
+functions(2).vector = @f2Problema1;
+
+aSolucao = fNewtonSistemasNaoLineares( aInicial, functions )
+
+# Podemos perturbar a os coeficientes aSolucao, 10% para cima ou -10% para baixo.
+# Caso ele esteja em um ponto de minimo, descolar um pouco para esquerda (*0.9) ou para direita (*1.1)
+# o valor do desvio vai subir.
+aSolucao = aSolucao.*0.9
 
 # Os pontos podem não estar ordenados, portanto calculamos os intervalos a partir dos pontos
 # mínimo e máximo de `x`.
@@ -30,10 +38,18 @@ yInterPontosAproximado = aSolucao(1) * sin( xInterpontos*aSolucao(2) );
 plot( x, y, '*', xInterpontos, yInterPontosAproximado );
 
 
-# Desvio quadrático médio = sum(d.^2) / m
 # Desvio                  = sum( abs(d) )
 
+DesvioLocal =
 
+# A se a média dos desvios for zero, então temos que o desvio quadrático está correto, pois
+# partimos do principio:
+#
+#     Usando a(1) e a(2) de modo que D seja mínimo:
+#     desvioLocal = a(1)*x(k) + a(2)*cos(x(k)) - y(k)
+#     D( a(1), a(2) ) = \sum_k=1^m ( desvioLocal )^2
+#
+# Desvio quadrático médio = sum(d.^2) / m
 
 
 
