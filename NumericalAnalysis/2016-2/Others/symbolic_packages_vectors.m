f39eff34eb9693681ed91af4a0e3b23f0eee01b1
evandrocoan <evandrocoan@hotmail.com>
2016-11-28 09:00:32 -0200

    Added the symbolic_packages_vectors.m coding example.

---
+++ NumericalAnalysis/2016-2/Others/symbolic_packages_vectors.m
@@ -0,0 +1,47 @@
+
+
+b(1,1).vector = @sin;
+b(1,2).vector = @cos;
+b(2,1).vector = @sec;
+b(2,2).vector = @csc;
+
+b
+b.vector
+
+printf( "\n\nCalling each element:\n" )
+b(1,1).vector
+b(1,2).vector
+b(2,1).vector
+b(2,2).vector
+
+printf( "\nCalculatin the sin of 1:\n" )
+b(1,1).vector(1)
+
+
+#
+# How do I declare a symbolic matrix in Octave?
+#
+# http://stackoverflow.com/questions/2472486/how-do-i-declare-a-symbolic-matrix-in-octave/40824167#40824167
+# http://stackoverflow.com/a/40824167/4934640
+#
+# octave> pkg install -forge symbolic
+#
+pkg load symbolic;
+
+b(1,1).vector = sym("a");
+b(1,2).vector = sym("b");
+b(2,1).vector = sym("c");
+b(2,2).vector = sym("d");
+
+b
+b.vector
+
+printf( "\n\nCalling each element:\n" )
+b(1,1).vector
+b(1,2).vector
+b(2,1).vector
+b(2,2).vector
+
+
+
+
