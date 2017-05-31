c75df00f10070c19a9a924052e1e2b48bb8af224
evandrocoan <evandrocoan@hotmail.com>
2017-05-31 20:29:17 -0300

    Moved the test.py example to pyqt4_examples/matplotlib.py

--- pyqt4_examples/matplotlib.py
+++ pyqt4_examples/matplotlib.py
@@ -6,9 +6,12 @@ import matplotlib.pyplot as plt
 
 x = random.sample(range(1000), 30)
 xbins = [0, len(x)]
+
+print( "x: " + str( x ) )
+print( "xbins: " + str( xbins ) )
+
 plt.bar(range(0,30), x)
 plt.show()
-
 
 
 
