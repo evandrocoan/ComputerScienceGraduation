85a93ad0c8a03c54be0ac4c60edcbccb937ffdb8
evandrocoan <evandrocoan@hotmail.com>
2017-08-24 14:10:48 -0300

    Finished the ThirdClassFixturesReuse/HamcrestTests.java

--- SoftwareTesting/ThirdClassFixturesReuse/HamcrestTests.java
+++ SoftwareTesting/ThirdClassFixturesReuse/HamcrestTests.java
@@ -2,46 +2,89 @@
 import org.junit.Test;
 import junit.framework.TestCase;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class HamcrestTests extends TestCase
+import br.ufsc.ine.leb.projetos.estoria.Fixture;
+import br.ufsc.ine.leb.projetos.estoria.FixtureSetup;
+import br.ufsc.ine.leb.sistemaBancario.Agencia;
+import br.ufsc.ine.leb.sistemaBancario.Conta;
+import br.ufsc.ine.leb.sistemaBancario.ValorMonetario;
+
+@FixtureSetup( TesteBancoCase2.class )
+public class HamcrestTests
 {
     @Test
     public void testEqualsObjectMatcher()
     {
-        String theBiscuit = new String( "Ginger" );
-        String myBiscuit = new String( "Ginger" );
+        final String theBiscuit = new String( "Ginger" );
+        final String myBiscuit = new String( "Ginger" );
 
-        assertThat( theBiscuit, equalTo( myBiscuit ) );
+        MatcherAssert.assertThat( theBiscuit, Matchers.equalTo( myBiscuit ) );
     }
 
     @Test
     public void testEqualsNumberMatcher()
     {
-        int theBiscuit = 5;
-        int myBiscuit = 4;
+        final int theBiscuit = 5;
+        final int myBiscuit = 4;
 
-        assertThat( theBiscuit, greaterThan( myBiscuit ) );
+        MatcherAssert.assertThat( theBiscuit, Matchers.greaterThan( myBiscuit ) );
     }
 
     @Test
     public void testEqualsTextMatcher()
     {
-        String theBiscuit = new String( "First Part" );
-        String myBiscuit = new String( "Part" );
+        final String theBiscuit = new String( "First Part" );
+        final String myBiscuit = new String( "Part" );
 
-        assertThat( theBiscuit, endsWith( myBiscuit ) );
+        MatcherAssert.assertThat( theBiscuit, Matchers.endsWith( myBiscuit ) );
     }
 
     @Test
     public void testEqualsArrayMatcher()
     {
-        String myBiscuit = "myValue";
-        String array[] = new String[] {
-                    "foo", "bar"
+        final String[] myBiscuit = new String[] {
+            "foo", "bar"
         };
+        MatcherAssert.assertThat( myBiscuit, Matchers.arrayWithSize( 2 ) );
+    }
 
-        assertThat( myBiscuit, allOf( startsWith( "my" ), containsString( "Val" ) ) );
+    @Test
+    public void testEqualsAllOfMatcher()
+    {
+        final String myBiscuit = "myValue";
+        MatcherAssert.assertThat( myBiscuit, Matchers.allOf( Matchers.startsWith( "my" ), Matchers.containsString( "Val" ) ) );
+    }
+
+    @Fixture
+    private Agencia agenciaCentro;
+
+    @Test
+    public void contaAllTests()
+    {
+        final Conta contaMaria = this.agenciaCentro.criarConta( "Maria" );
+        final ValorMonetario saldoEsperado = Helper.criarValorMonetario( 0 );
+
+        final String nomeEsperado = "Maria";
+        final String identificadorEsperado = "0001-5";
+
+        // Assert.assertNotNull( contaMaria );
+        MatcherAssert.assertThat( contaMaria, Matchers.is( Matchers.notNullValue( Conta.class ) ) );
+
+        // Assert.assertEquals( identificadorEsperado,  );
+        MatcherAssert.assertThat( identificadorEsperado, Matchers.equalTo( contaMaria.obterIdentificador() ) );
+
+        // Assert.assertEquals( nomeEsperado, contaMaria.obterTitular() );
+        MatcherAssert.assertThat( nomeEsperado, Matchers.equalTo( contaMaria.obterTitular() ) );
+
+        // Assert.assertEquals( saldoEsperado, contaMaria.calcularSaldo() );
+        MatcherAssert.assertThat( saldoEsperado, Matchers.equalTo( contaMaria.calcularSaldo() ) );
+
+        // Assert.assertEquals( this.agenciaCentro, contaMaria.obterAgencia() );
+        MatcherAssert.assertThat( this.agenciaCentro, Matchers.equalTo( contaMaria.obterAgencia() ) );
     }
 }
