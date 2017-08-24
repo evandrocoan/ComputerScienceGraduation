4b4a432d031b8f1cd8c37f2e777efd0ebdcf1bff
evandrocoan <evandrocoan@hotmail.com>
2017-08-24 11:49:20 -0300

    Added the third class second part exercises.

---
+++ SoftwareTesting/ThirdClassFixturesReuse/HamcrestTests.java
@@ -0,0 +1,47 @@
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class HamcrestTests extends TestCase
+{
+    @Test
+    public void testEqualsObjectMatcher()
+    {
+        String theBiscuit = new String( "Ginger" );
+        String myBiscuit = new String( "Ginger" );
+        
+        assertThat( theBiscuit, equalTo( myBiscuit ) );
+    }
+    
+    @Test
+    public void testEqualsNumberMatcher()
+    {
+        int theBiscuit = 5;
+        int myBiscuit = 4;
+        
+        assertThat( theBiscuit, greaterThan( myBiscuit ) );
+    }
+    
+    @Test
+    public void testEqualsTextMatcher()
+    {
+        String theBiscuit = new String( "First Part" );
+        String myBiscuit = new String( "Part" );
+        
+        assertThat( theBiscuit, endsWith( myBiscuit ) );
+    }
+    
+    @Test
+    public void testEqualsArrayMatcher()
+    {
+        String myBiscuit = "myValue";
+        String array[] = new String[] {
+                    "foo", "bar"
+        };
+        
+        assertThat( myBiscuit, allOf( startsWith( "my" ), containsString( "Val" ) ) );
+    }
+}
