1ad322c3e7bb541c4f972c2c1590cb4fac84e403
evandrocoan <evandrocoan@hotmail.com>
2018-08-07 17:59:27 -0300

    Updated OperatingSystems/TeachingEpos/app/compile_remotely.sh

--- OperatingSystems/TeachingEpos/app/compile_remotely.sh
+++ OperatingSystems/TeachingEpos/app/compile_remotely.sh
@@ -4,13 +4,16 @@
 # Import the helper functions.
 . ./../timer_calculator.sh
 
+pushd `dirname $0` > /dev/null
+SCRIPT_FOLDER_PATH=`pwd`
+popd > /dev/null
 
 # sync files, usage example: sshpass -p 123 rsync -rvu ../../TeachingEpos/* user.name@so.user.name.vms.ufsc.br:~/2016-2_OperatingSystems/TeachingEpos
 # Use this on your '~/.bashrc' file:
 # export EPOS_COMPILER_MACHINE_PASS=123
 # export EPOS_COMPILER_MACHINE_ADDRESS=user.name@so.user.name.vms.ufsc.br
 #
-sshpass -p $EPOS_COMPILER_MACHINE_PASS rsync -rvu ../../TeachingEpos/* $EPOS_COMPILER_MACHINE_ADDRESS:~/OperatingSystems/TeachingEpos
+sshpass -p $EPOS_COMPILER_MACHINE_PASS rsync -rvu $SCRIPT_FOLDER_PATH/../../TeachingEpos/* $EPOS_COMPILER_MACHINE_ADDRESS:~/OperatingSystems/TeachingEpos
 
 # sshpass -p $EPOS_COMPILER_MACHINE_PASS unison ../../TeachingEpos ssh://$EPOS_COMPILER_MACHINE_ADDRESS//home/evandro.coan/OperatingSystems/TeachingEpos
 
