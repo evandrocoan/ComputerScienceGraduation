cd82e54f6814eca08a26625103412ca1be12b108
evandrocoan <evandrocoan@hotmail.com>
2018-08-11 15:33:18 -0300

    Fixed compile_remotely.sh not creating the remove directory on
    the first run.

--- OperatingSystems/TeachingEpos/app/compile_remotely.sh
+++ OperatingSystems/TeachingEpos/app/compile_remotely.sh
@@ -13,7 +13,12 @@ popd > /dev/null
 # export EPOS_COMPILER_MACHINE_PASS=123
 # export EPOS_COMPILER_MACHINE_ADDRESS=user.name@so.user.name.vms.ufsc.br
 #
-sshpass -p $EPOS_COMPILER_MACHINE_PASS rsync -rvu $SCRIPT_FOLDER_PATH/../../TeachingEpos/* $EPOS_COMPILER_MACHINE_ADDRESS:~/OperatingSystems/TeachingEpos
+# EPOS_COMPILER_MACHINE_PASS=admin123
+# EPOS_COMPILER_MACHINE_ADDRESS=linux@127.0.0.1
+
+TARGET_DIRECTORY='~/OperatingSystems/TeachingEpos'
+sshpass -p $EPOS_COMPILER_MACHINE_PASS ssh $EPOS_COMPILER_MACHINE_ADDRESS mkdir -p $TARGET_DIRECTORY
+sshpass -p $EPOS_COMPILER_MACHINE_PASS rsync -rvu $SCRIPT_FOLDER_PATH/../../TeachingEpos/* $EPOS_COMPILER_MACHINE_ADDRESS:$TARGET_DIRECTORY
 
 # https://tech.tiq.cc/2016/04/how-to-use-unison-for-automated-two-way-file-synchronization-on-linux-ubuntu-and-windows-and-android/
 # sshpass -p $EPOS_COMPILER_MACHINE_PASS unison $SCRIPT_FOLDER_PATH/../../TeachingEpos ssh://$EPOS_COMPILER_MACHINE_ADDRESS//home/evandro.coan/OperatingSystems/TeachingEpos -auto
@@ -23,7 +28,8 @@ sshpass -p $EPOS_COMPILER_MACHINE_PASS rsync -rvu $SCRIPT_FOLDER_PATH/../../Teac
 APPLICATION_TO_RUN=$(echo $1 | cut -d'.' -f 1)
 
 
-REMOTE_COMMAND_TO_RUN="cd /home/evandro.coan/OperatingSystems/TeachingEpos;
+# REMOTE_COMMAND_TO_RUN="cd /home/evandro.coan/OperatingSystems/TeachingEpos;
+REMOTE_COMMAND_TO_RUN="cd $TARGET_DIRECTORY;
 pwd;
 sh compile_and_run.sh $APPLICATION_TO_RUN;"
 
