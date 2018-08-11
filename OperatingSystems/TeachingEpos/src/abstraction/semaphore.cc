f319fa528f14572e0dec4df81195367d3e62c12b
evandrocoan <evandrocoan@hotmail.com>
2018-08-10 21:25:36 -0300

    Deprecated this usage on src/abstraction/semaphore.cc

--- OperatingSystems/TeachingEpos/src/abstraction/semaphore.cc
+++ OperatingSystems/TeachingEpos/src/abstraction/semaphore.cc
@@ -6,7 +6,7 @@ __BEGIN_SYS
 
 Semaphore::Semaphore(int v): _value(v)
 {
-    db<Synchronizer>(TRC) << "Semaphore(value=" << this->_value << ") => " << this << endl;
+    db<Synchronizer>(TRC) << "Semaphore(value=" << _value << ") => " << this << endl;
 }
 
 
@@ -18,43 +18,43 @@ Semaphore::~Semaphore()
 
 void Semaphore::p()
 {
-    db<Synchronizer>(TRC) << "Semaphore::p(this=" << this << ",value=" << this->_value << ")" << endl;
+    db<Synchronizer>(TRC) << "Semaphore::p(this=" << this << ",value=" << _value << ")" << endl;
 
     // Disable all interrupts
-    this->begin_atomic();
+    begin_atomic();
 
-    if(this->_value > 0)
+    if(_value > 0)
     {
-        fdec(this->_value);
-        this->end_atomic();
+        fdec(_value);
+        end_atomic();
         return;
     }
 
-    this->threads_em_espera.insert(&Thread::_running->_link);
+    threads_em_espera.insert(&Thread::_running->_link);
     sleep();
-    this->end_atomic();
+    end_atomic();
 }
 
 
 void Semaphore::v()
 {
-    db<Synchronizer>(TRC) << "Semaphore::v(this=" << this << ",value=" << this->_value << ")" << endl;
+    db<Synchronizer>(TRC) << "Semaphore::v(this=" << this << ",value=" << _value << ")" << endl;
 
     // Disable all interrupts
-    this->begin_atomic();
+    begin_atomic();
 
-    if(this->threads_em_espera.empty())
+    if(threads_em_espera.empty())
     {
-        finc(this->_value);
+        finc(_value);
     }
     else
     {
-        EPOS::S::U::List_Elements::Doubly_Linked_Ordered<EPOS::S::Thread, unsigned int>* running_thread_link = this->threads_em_espera.remove();
+        EPOS::S::U::List_Elements::Doubly_Linked_Ordered<EPOS::S::Thread, unsigned int>* running_thread_link = threads_em_espera.remove();
 
         // put thread on the ready queue
         Thread::_ready.insert(running_thread_link);
     }
-    this->end_atomic();
+    end_atomic();
 }
 
 __END_SYS
