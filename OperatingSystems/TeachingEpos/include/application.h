8fbc9c0ec69958571a5af3a5958bdf86cc75e6dc
evandrocoan <evandrocoan@hotmail.com>
2018-09-09 14:47:30 -0300

    Added documentation comment on application.h

--- OperatingSystems/TeachingEpos/include/application.h
+++ OperatingSystems/TeachingEpos/include/application.h
@@ -16,6 +16,12 @@ __BEGIN_SYS
 class Application
 {
     friend class Init_Application;
+
+    /**
+     * What is the meaning of prepended double colon “::”? This ensures that resolution occurs from
+     * the global namespace, instead of starting at the namespace you're currently in.
+     * https://stackoverflow.com/questions/4269034/what-is-the-meaning-of-prepended-double-colon
+     */
     friend void * ::malloc(size_t);
     friend void ::free(void *);
 
